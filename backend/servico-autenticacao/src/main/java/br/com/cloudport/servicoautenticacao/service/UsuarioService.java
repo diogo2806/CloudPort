2dc164574a0829d9cecb1fbca6de7bf47f2221ed
diogo2806 <diogo_2806@hotmail.com>
2023-07-14 19:55:19 +0000

    controller
    Signed-off-by: diogo2806 <diogo_2806@hotmail.com>

--- backend/servico-autenticacao/src/main/java/br/com/cloudport/servicoautenticacao/service/UsuarioService.java
+++ backend/servico-autenticacao/src/main/java/br/com/cloudport/servicoautenticacao/service/UsuarioService.java
@@ -6,7 +6,6 @@ import br.com.cloudport.servicoautenticacao.repository.UsuarioRepository;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
 import java.util.stream.Collectors;
 

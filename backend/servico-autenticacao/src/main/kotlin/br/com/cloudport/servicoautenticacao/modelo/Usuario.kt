b6b2812473153e78ca42539e6e365422d142dfe5
diogo2806 <diogo_2806@hotmail.com>
2023-07-14 16:12:01 +0000

    build
    Signed-off-by: diogo2806 <diogo_2806@hotmail.com>

--- backend/servico-autenticacao/src/main/kotlin/br/com/cloudport/servicoautenticacao/modelo/Usuario.kt
+++ backend/servico-autenticacao/src/main/kotlin/br/com/cloudport/servicoautenticacao/modelo/Usuario.kt
@@ -1,11 +1,7 @@
 package br.com.cloudport.servicoautenticacao.modelo
 
-import javax.persistence.Entity
-import javax.persistence.GeneratedValue
-import javax.persistence.GenerationType
-import javax.persistence.Id
-import javax.persistence.Enumerated
-import javax.persistence.EnumType
+import javax.persistence.*
+
 import java.time.LocalDate
 import br.com.cloudport.servicoautenticacao.modelo.StatusCadastro
 

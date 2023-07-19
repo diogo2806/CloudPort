692458dcf5194fe5d6b63b497c76eb183304b468
diogo2806 <diogo_2806@hotmail.com>
2023-07-19 17:00:23 +0000

    update
    Signed-off-by: diogo2806 <diogo_2806@hotmail.com>

--- backend/servico-autenticacao/src/main/java/br/com/cloudport/servicoautenticacao/controller/AprovacaoAcessoController.java
+++ backend/servico-autenticacao/src/main/java/br/com/cloudport/servicoautenticacao/controller/AprovacaoAcessoController.java
@@ -5,7 +5,7 @@ import br.com.cloudport.servicoautenticacao.model.SolicitacaoAcesso;
 import br.com.cloudport.servicoautenticacao.service.SolicitacaoAcessoService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
+//import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
--- frontend/cloudport/src/app/servico-autenticacao/cadastro-usuario/cadastro-usuario.component.spec.ts
+++ frontend/cloudport/src/app/servico-autenticacao/cadastro-usuario/cadastro-usuario.component.spec.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { CadastroUsuarioService } from './cadastro-usuario-service/cadastro-usuario-service.service';
 import { UsuarioDTO } from '../models/usuario-dto';
+import { NotificacaoService } from '../../notificacao/notificacao.service'; // Importe o serviço de notificação
 
 @Component({
   selector: 'app-cadastro-usuario',
@@ -10,16 +11,21 @@ import { UsuarioDTO } from '../models/usuario-dto';
 export class CadastroUsuarioComponent {
   usuario: UsuarioDTO = new UsuarioDTO();
 
-  constructor(private cadastroUsuarioService: CadastroUsuarioService) { }
+  constructor(
+    private cadastroUsuarioService: CadastroUsuarioService,
+    private notificacaoService: NotificacaoService
+    ) { }
 
   onSubmit() {
     this.cadastroUsuarioService.salvarUsuario(this.usuario)
       .subscribe(result => {
         // Tratar resultado
         console.log(result);
+        this.notificacaoService.show('Usuário cadastrado com sucesso!'); // Mostre a notificação
       }, error => {
         // Tratar erro
         console.log(error);
+        this.notificacaoService.show('Ocorreu um erro ao cadastrar o usuário.'); // Mostre uma notificação de erro
       });
   }
 }

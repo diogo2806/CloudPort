92e84c19d4fb198cf38ff0b1174b097cba6f2b3f
diogo2806 <diogo_2806@hotmail.com>
2023-07-21 15:54:29 +0000

    update
    Signed-off-by: diogo2806 <diogo_2806@hotmail.com>

--- frontend/cloudport/src/app/servico-autenticacao/login/login.component.ts
+++ frontend/cloudport/src/app/servico-autenticacao/login/login.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { first } from 'rxjs/operators';
+import { TranslateService } from '@ngx-translate/core';
+
 
 import { AuthenticationService } from '../../service/login-service/AuthenticationService';
 

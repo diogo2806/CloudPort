e57465037051e703f0cf7eeef53e6039a9bb21cc
diogo2806 <diogo_2806@hotmail.com>
2023-07-31 19:54:07 +0000

    update
    Signed-off-by: diogo2806 <diogo_2806@hotmail.com>

--- frontend/cloudport/src/app/componentes/role/role.component.ts
+++ frontend/cloudport/src/app/componentes/role/role.component.ts
@@ -1,24 +1,26 @@
-import { Component } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { environment } from '../service/endpoint';
 import { AuthenticationService } from '../service/servico-autenticacao/authentication.service';
-import { HttpHeaders } from '@angular/common/http';
-
 
 @Component({
   selector: 'app-role',
   templateUrl: './role.component.html',
   styleUrls: ['./role.component.css']
 })
-export class RoleComponent {
+export class RoleComponent implements OnInit {
 
   roleName: string = "";
+  roles: any[] = []; // replace any with your actual role type
 
   constructor(private http: HttpClient, private authenticationService: AuthenticationService) { }
 
-  createRole() {
+  ngOnInit() {
+    this.loadRoles();
+  }
 
-    const token = this.authenticationService.currentUserValue?.token; // Replace with your actual token
+  loadRoles() {
+    const token = this.authenticationService.currentUserValue?.token;
 
     const httpOptions = {
       headers: new HttpHeaders({
@@ -27,10 +29,26 @@ export class RoleComponent {
       })
     };
 
+    this.http.get<any[]>(`${environment.role.list}`, httpOptions)
+      .subscribe(response => {
+        this.roles = response;
+      });
+  }
+
+  createRole() {
+    const token = this.authenticationService.currentUserValue?.token;
+
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type':  'application/json',
+        'Authorization': 'Bearer ' + token
+      })
+    };
 
     this.http.post(`${environment.role.create}`, { name: this.roleName }, httpOptions)
       .subscribe(response => {
         console.log('Role created:', response);
+        this.loadRoles(); // Reload the list of roles after a new role is created
       });
   }
 }

d5a19227d9e25c01321b3fd1a8858a0d835b92e1
diogo2806 <diogo_2806@hotmail.com>
2023-08-08 02:49:59 +0000

    update
    Signed-off-by: diogo2806 <diogo_2806@hotmail.com>

--- frontend/cloudport/src/app/componentes/navbar/TabService.ts
+++ frontend/cloudport/src/app/componentes/navbar/TabService.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
-
 @Injectable({
   providedIn: 'root'
 })
@@ -8,16 +7,32 @@ export class TabService {
   private tabsSubject = new BehaviorSubject<string[]>([]);
   tabs$ = this.tabsSubject.asObservable();
 
-  openTab(tab: string) {
+  // Armazenamento para o conteúdo de cada aba
+  private tabContents: { [tabName: string]: any } = {};
+
+  openTab(tab: string, content?: any) {
     const tabs = this.tabsSubject.value;
     if (!tabs.includes(tab)) {
       this.tabsSubject.next([...tabs, tab]);
+      if (content) {
+        this.tabContents[tab] = content;
+      }
     }
   }
 
   closeTab(tab: string) {
     const tabs = this.tabsSubject.value;
     this.tabsSubject.next(tabs.filter(t => t !== tab));
-    
+    // Remova o conteúdo da aba quando ela for fechada
+    delete this.tabContents[tab];
+  }
+
+  // Métodos para gerenciar o conteúdo da aba
+  getTabContent(tab: string): any {
+    return this.tabContents[tab];
+  }
+
+  setTabContent(tab: string, content: any) {
+    this.tabContents[tab] = content;
   }
 }

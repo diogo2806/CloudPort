97484a00cc05d59b10eaa6fa9daf49f1426cfcf1
Diogo Santos <diogo_2806@hotmail.com>
2025-06-26 19:23:33 -0300

    Add token generation and validation test

--- backend/servico-autenticacao/src/test/java/br/com/cloudport/servicoautenticacao/ServicoAutenticacaoApplicationTests.java
+++ backend/servico-autenticacao/src/test/java/br/com/cloudport/servicoautenticacao/ServicoAutenticacaoApplicationTests.java
@@ -1,13 +1,31 @@
 package br.com.cloudport.servicoautenticacao;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.*;
+
+import br.com.cloudport.servicoautenticacao.config.TokenService;
+import br.com.cloudport.servicoautenticacao.model.User;
+
+import java.util.HashSet;
 
 @SpringBootTest
 class ServicoAutenticacaoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private TokenService tokenService;
+
+    @Test
+    void contextLoads() {
+    }
+
+    @Test
+    void generateAndValidateToken() {
+        User user = new User("test", "pass", new HashSet<>());
+        String token = tokenService.generateToken(user);
+        assertNotNull(token);
+        assertEquals(user.getLogin(), tokenService.validateToken(token));
+    }
 
 }

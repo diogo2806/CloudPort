aa6e1566f39df7a183e292e699717183dbf0adcf
Diogo Santos <diogo_2806@hotmail.com>
2025-06-26 19:23:21 -0300

    Use UUID in UserRepository

--- backend/servico-autenticacao/src/main/java/br/com/cloudport/servicoautenticacao/repositories/UserRepository.java
+++ backend/servico-autenticacao/src/main/java/br/com/cloudport/servicoautenticacao/repositories/UserRepository.java
@@ -3,8 +3,9 @@ package br.com.cloudport.servicoautenticacao.repositories;
 import br.com.cloudport.servicoautenticacao.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
+import java.util.UUID;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, UUID> {
     Optional<User> findByLogin(String login);
 }
 

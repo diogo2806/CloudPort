e99332275d3572d466f4beb1a66c376cefedd63e
Diogo Santos <diogo_2806@hotmail.com>
2025-06-26 19:23:16 -0300

    Remove outdated comments from RoleController

--- backend/servico-autenticacao/src/main/java/br/com/cloudport/servicoautenticacao/controllers/RoleController.java
+++ backend/servico-autenticacao/src/main/java/br/com/cloudport/servicoautenticacao/controllers/RoleController.java
@@ -1,7 +1,7 @@
 package br.com.cloudport.servicoautenticacao.controller;
 
 import br.com.cloudport.servicoautenticacao.model.Role;
-import br.com.cloudport.servicoautenticacao.dto.RoleDTO; // Note o "R" maiúsculo
+import br.com.cloudport.servicoautenticacao.dto.RoleDTO;
 import br.com.cloudport.servicoautenticacao.service.RoleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -20,14 +20,14 @@ public class RoleController {
     }
 
     @PostMapping
-    public ResponseEntity<RoleDTO> createRole(@RequestBody RoleDTO roleDTO) { // Note o "R" maiúsculo
-        RoleDTO savedRole = roleService.saveRole(roleDTO); // Note o "R" maiúsculo
+    public ResponseEntity<RoleDTO> createRole(@RequestBody RoleDTO roleDTO) {
+        RoleDTO savedRole = roleService.saveRole(roleDTO);
         return ResponseEntity.ok(savedRole);
     }
 
     @GetMapping("/{name}")
-    public ResponseEntity<RoleDTO> getRole(@PathVariable String name) { // Note o "R" maiúsculo
-        RoleDTO role = roleService.findByName(name); // Note o "R" maiúsculo
+    public ResponseEntity<RoleDTO> getRole(@PathVariable String name) {
+        RoleDTO role = roleService.findByName(name);
         return ResponseEntity.ok(role);
     }
 

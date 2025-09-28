c7a10427bd0a77acb50164b9c726a5d4f8a75d88
Diogo Santos <diogo_2806@hotmail.com>
2025-09-28 07:02:25 -0300

    Corrige persistência do token do usuário

--- frontend/cloudport/src/app/componentes/service/servico-autenticacao/jwt.interceptor.ts
+++ frontend/cloudport/src/app/componentes/service/servico-autenticacao/jwt.interceptor.ts
@@ -12,11 +12,11 @@ export class JwtInterceptor implements HttpInterceptor {
     intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         // add authorization header with jwt token if available
         let currentUser:any = this.authenticationService.currentUserValue;
-        
-        if (currentUser && currentUser.data.token) {
+
+        if (currentUser && currentUser.token) {
             request = request.clone({
                 setHeaders: {
-                    Authorization: `Bearer ${currentUser.data.token}`
+                    Authorization: `Bearer ${currentUser.token}`
                 }
             });
         }

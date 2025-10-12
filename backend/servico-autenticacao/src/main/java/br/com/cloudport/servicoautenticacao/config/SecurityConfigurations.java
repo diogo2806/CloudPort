e93794caa1651b85de0a5279a1e9b89a7644dc6f
Diogo Santos <diogo_2806@hotmail.com>
2025-10-12 08:04:43 -0300

    Adjust security config for Spring Security 5

--- backend/servico-autenticacao/src/main/java/br/com/cloudport/servicoautenticacao/config/SecurityConfigurations.java
+++ backend/servico-autenticacao/src/main/java/br/com/cloudport/servicoautenticacao/config/SecurityConfigurations.java
@@ -36,20 +36,21 @@ public class SecurityConfigurations {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        return  httpSecurity
-                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
-                .csrf(csrf -> csrf.disable())
-                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/auth/**").permitAll()
-                        .requestMatchers(HttpMethod.POST, "/auth").permitAll()
-                        .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
-                        .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
-                        .requestMatchers(HttpMethod.POST, "/product").hasRole("ADMIN")
-                        .anyRequest().authenticated()
-                )
-                .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
-                .build();
+        httpSecurity
+                .cors().configurationSource(corsConfigurationSource()).and()
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+                .authorizeRequests()
+                    .antMatchers("/auth/**").permitAll()
+                    .antMatchers(HttpMethod.POST, "/auth").permitAll()
+                    .antMatchers(HttpMethod.POST, "/auth/login").permitAll()
+                    .antMatchers(HttpMethod.POST, "/auth/register").permitAll()
+                    .antMatchers(HttpMethod.POST, "/product").hasRole("ADMIN")
+                    .anyRequest().authenticated()
+                .and()
+                .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return httpSecurity.build();
     }
 
     @Bean

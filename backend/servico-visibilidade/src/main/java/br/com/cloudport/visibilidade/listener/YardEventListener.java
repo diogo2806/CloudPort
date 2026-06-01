01fb34544093438745a42ef287138c603712ac10
Diogo Santos <diogo_2806@hotmail.com>
2026-06-01 10:03:20 -0300

    feat(visibilidade): implementar lógica real no YardEventListener

--- backend/servico-visibilidade/src/main/java/br/com/cloudport/visibilidade/listener/YardEventListener.java
+++ backend/servico-visibilidade/src/main/java/br/com/cloudport/visibilidade/listener/YardEventListener.java
@@ -1,32 +1,56 @@
 package br.com.cloudport.visibilidade.listener;
 
 import br.com.cloudport.visibilidade.config.RabbitMQConfig;
+import br.com.cloudport.visibilidade.entity.ConteinerLocalizacao;
+import br.com.cloudport.visibilidade.repository.ConteinerLocalizacaoRepository;
+import br.com.cloudport.visibilidade.service.CapacidadeYardService;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDateTime;
 import java.util.Map;
 
 @Component
 public class YardEventListener {
 
+    @Autowired
+    private ConteinerLocalizacaoRepository localizacaoRepository;
+
+    @Autowired
+    private CapacidadeYardService capacidadeYardService;
+
     @RabbitListener(queues = RabbitMQConfig.VISIBILIDADE_YARD_QUEUE)
     public void handleYardEvent(Map<String, Object> event) {
         String eventType = (String) event.get("eventType");
-        System.out.println("[YardEventListener] Evento recebido: " + eventType);
+        String containerId = (String) event.get("containerId");
+        String zona = (String) event.get("zona");
+        String posicao = (String) event.get("posicao");
+
+        if (containerId == null) return;
 
         if ("yard.container.stored".equals(eventType)) {
-            // TODO: Atualizar ConteinerLocalizacao com zona e posição
-            System.out.println("Container armazenado no yard");
+            ConteinerLocalizacao loc = localizacaoRepository.findByContainerId(containerId)
+                    .orElseGet(() -> {
+                        ConteinerLocalizacao nova = new ConteinerLocalizacao();
+                        nova.setContainerId(containerId);
+                        return nova;
+                    });
+
+            loc.setStatusAtual("no_yard");
+            loc.setZona(zona);
+            loc.setPosicao(posicao);
+            loc.setDataAtualizacao(LocalDateTime.now());
+            localizacaoRepository.save(loc);
+
+            System.out.println("[Yard] Container " + containerId + " armazenado em " + zona + "-" + posicao);
         }
 
-        if ("yard.container.retrieved".equals(eventType)) {
-            // TODO: Atualizar status para aguardando_saida
-            System.out.println("Container retirado do yard");
-        }
-
-        if ("yard.capacity_updated".equals(eventType)) {
-            // TODO: Atualizar CapacidadeYard
-            System.out.println("Capacidade do yard atualizada");
+        if ("yard.capacity_updated".equals(eventType) && zona != null) {
+            Integer ocupacao = (Integer) event.get("ocupacaoAtual");
+            if (ocupacao != null) {
+                capacidadeYardService.atualizarOcupacao(zona, ocupacao);
+            }
         }
     }
 }

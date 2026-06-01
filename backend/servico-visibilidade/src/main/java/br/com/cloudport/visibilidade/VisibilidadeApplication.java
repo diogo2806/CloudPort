da82d416a35ea1ff39b1514f8acad3c8aaaebf8b
Diogo Santos <diogo_2806@hotmail.com>
2026-06-01 09:46:39 -0300

    feat(visibilidade): adicionar VisibilidadeApplication.java (main class)

---
+++ backend/servico-visibilidade/src/main/java/br/com/cloudport/visibilidade/VisibilidadeApplication.java
@@ -0,0 +1,13 @@
+package br.com.cloudport.visibilidade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisibilidadeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VisibilidadeApplication.class, args);
+    }
+
+}

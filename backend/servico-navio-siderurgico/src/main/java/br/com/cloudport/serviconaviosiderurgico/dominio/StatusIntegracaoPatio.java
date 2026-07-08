e00bba317af49b3e34ac206616b079a2df515960
Diogo Santos <diogo_2806@hotmail.com>
2026-07-08 18:39:46 -0300

    feat: integra visita de navio com patio

---
+++ backend/servico-navio-siderurgico/src/main/java/br/com/cloudport/serviconaviosiderurgico/dominio/StatusIntegracaoPatio.java
@@ -0,0 +1,11 @@
+package br.com.cloudport.serviconaviosiderurgico.dominio;
+
+public enum StatusIntegracaoPatio {
+    NAO_GERADO,
+    RESERVADO,
+    ORDEM_GERADA,
+    EM_EXECUCAO,
+    SINCRONIZADO,
+    ERRO,
+    CANCELADO
+}

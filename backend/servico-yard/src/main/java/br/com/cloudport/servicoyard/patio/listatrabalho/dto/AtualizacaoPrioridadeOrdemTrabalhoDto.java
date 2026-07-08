a9b083cd4d3b585669c0de2f8ca5ee5e722c09f1
Diogo Santos <diogo_2806@hotmail.com>
2026-07-08 19:12:20 -0300

    feat: adiciona dto de prioridade de ordem de patio

---
+++ backend/servico-yard/src/main/java/br/com/cloudport/servicoyard/patio/listatrabalho/dto/AtualizacaoPrioridadeOrdemTrabalhoDto.java
@@ -0,0 +1,25 @@
+package br.com.cloudport.servicoyard.patio.listatrabalho.dto;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+
+public class AtualizacaoPrioridadeOrdemTrabalhoDto {
+
+    @NotNull
+    @Min(0)
+    private Integer prioridadeOperacional;
+
+    private Boolean prioridadeBusca;
+
+    public AtualizacaoPrioridadeOrdemTrabalhoDto() {
+    }
+
+    public Integer getPrioridadeOperacional() { return prioridadeOperacional; }
+    public void setPrioridadeOperacional(Integer prioridadeOperacional) { this.prioridadeOperacional = prioridadeOperacional; }
+    public Boolean getPrioridadeBusca() { return prioridadeBusca; }
+    public void setPrioridadeBusca(Boolean prioridadeBusca) { this.prioridadeBusca = prioridadeBusca; }
+
+    public boolean prioridadeBuscaEfetiva() {
+        return Boolean.TRUE.equals(prioridadeBusca);
+    }
+}

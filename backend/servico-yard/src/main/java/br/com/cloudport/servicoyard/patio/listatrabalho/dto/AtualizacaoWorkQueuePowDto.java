6c9039712ae95f2369747423753beb27909e598c
Diogo Santos <diogo_2806@hotmail.com>
2026-07-08 19:44:24 -0300

    Implementa work queues de patio para navio

---
+++ backend/servico-yard/src/main/java/br/com/cloudport/servicoyard/patio/listatrabalho/dto/AtualizacaoWorkQueuePowDto.java
@@ -0,0 +1,12 @@
+package br.com.cloudport.servicoyard.patio.listatrabalho.dto;
+
+public class AtualizacaoWorkQueuePowDto {
+
+    private String pow;
+    private String poolOperacional;
+
+    public String getPow() { return pow; }
+    public void setPow(String pow) { this.pow = pow; }
+    public String getPoolOperacional() { return poolOperacional; }
+    public void setPoolOperacional(String poolOperacional) { this.poolOperacional = poolOperacional; }
+}

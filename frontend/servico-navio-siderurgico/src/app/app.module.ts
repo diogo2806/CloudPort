87cc9e1e3dc5981e82d1ef6c90d6382ed4b40128
Diogo Santos <diogo_2806@hotmail.com>
2026-07-15 18:37:18 -0300

    feat: registrar autenticação HTTP no Control Room

--- frontend/servico-navio-siderurgico/src/app/app.module.ts
+++ frontend/servico-navio-siderurgico/src/app/app.module.ts
@@ -1,12 +1,16 @@
-import { HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import { JwtInterceptor } from './jwt.interceptor';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, HttpClientModule, FormsModule],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {}

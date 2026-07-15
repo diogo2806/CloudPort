ed252d5c1f958ca9be5bfb741f933d08061f7e4d
Diogo Santos <diogo_2806@hotmail.com>
2026-07-15 18:49:49 -0300

    feat: impedir duplicidade da projeção canônica de navio

--- backend/servico-navio-siderurgico/src/main/java/br/com/cloudport/serviconaviosiderurgico/repositorio/NavioSiderurgicoRepositorio.java
+++ backend/servico-navio-siderurgico/src/main/java/br/com/cloudport/serviconaviosiderurgico/repositorio/NavioSiderurgicoRepositorio.java
@@ -5,4 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface NavioSiderurgicoRepositorio extends JpaRepository<NavioSiderurgico, Long> {
     boolean existsByCodigoImoIgnoreCase(String codigoImo);
+    boolean existsByNavioCadastroId(Long navioCadastroId);
 }

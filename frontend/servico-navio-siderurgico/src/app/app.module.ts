116a197f78802bfd9c84f15ae2285d2f1404293b
Diogo Santos <diogo_2806@hotmail.com>
2026-07-15 18:38:47 -0300

    feat: inicializar o Control Room pelo gate de autenticação

--- frontend/servico-navio-siderurgico/src/app/app.module.ts
+++ frontend/servico-navio-siderurgico/src/app/app.module.ts
@@ -3,14 +3,15 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import { AuthGateComponent } from './auth-gate.component';
 import { JwtInterceptor } from './jwt.interceptor';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, AuthGateComponent],
   imports: [BrowserModule, HttpClientModule, FormsModule],
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true }
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [AuthGateComponent]
 })
 export class AppModule {}

02ea07099b68c3194b0e9ed97faa0364dfe7885b
Diogo Santos <diogo_2806@hotmail.com>
2026-07-16 00:03:50 -0300

    refactor: usar injecao por construtor no yard

--- backend/servico-visibilidade/src/main/java/br/com/cloudport/visibilidade/controller/YardController.java
+++ backend/servico-visibilidade/src/main/java/br/com/cloudport/visibilidade/controller/YardController.java
@@ -2,15 +2,20 @@ package br.com.cloudport.visibilidade.controller;
 
 import br.com.cloudport.visibilidade.entity.CapacidadeYard;
 import br.com.cloudport.visibilidade.service.CapacidadeYardService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/visibilidade/yard")
 public class YardController {
 
-    @Autowired
-    private CapacidadeYardService capacidadeYardService;
+    private final CapacidadeYardService capacidadeYardService;
+
+    public YardController(CapacidadeYardService capacidadeYardService) {
+        this.capacidadeYardService = capacidadeYardService;
+    }
 
     @GetMapping("/capacity")
     public CapacidadeYard getCapacity(@RequestParam String zona) {

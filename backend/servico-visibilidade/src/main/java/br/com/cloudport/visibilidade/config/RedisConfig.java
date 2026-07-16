82c7013c69f903f157630d30ace3951b26302e26
Diogo Santos <diogo_2806@hotmail.com>
2026-07-16 09:08:27 -0300

    fix: nomear cache da visibilidade

--- backend/servico-visibilidade/src/main/java/br/com/cloudport/visibilidade/config/RedisConfig.java
+++ backend/servico-visibilidade/src/main/java/br/com/cloudport/visibilidade/config/RedisConfig.java
@@ -1,5 +1,9 @@
 package br.com.cloudport.visibilidade.config;
 
+import java.time.Duration;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,8 +14,6 @@ import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
-
-import java.time.Duration;
 
 @Configuration
 @EnableCaching
@@ -28,8 +30,8 @@ public class RedisConfig {
         return template;
     }
 
-    @Bean
-    public RedisCacheManager cacheManager(RedisConnectionFactory connectionFactory) {
+    @Bean(name = "visibilidadeCacheManager")
+    public CacheManager visibilidadeCacheManager(RedisConnectionFactory connectionFactory) {
         RedisCacheConfiguration config = RedisCacheConfiguration.defaultCacheConfig()
                 .entryTtl(Duration.ofMinutes(5))
                 .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()))
@@ -39,5 +41,15 @@ public class RedisConfig {
         return RedisCacheManager.builder(connectionFactory)
                 .cacheDefaults(config)
                 .build();
+    }
+
+    @Bean(name = "cacheManager")
+    @ConditionalOnProperty(
+            name = "cloudport.runtime.general",
+            havingValue = "false",
+            matchIfMissing = true)
+    public CacheManager cacheManagerStandalone(
+            @Qualifier("visibilidadeCacheManager") CacheManager visibilidadeCacheManager) {
+        return visibilidadeCacheManager;
     }
 }

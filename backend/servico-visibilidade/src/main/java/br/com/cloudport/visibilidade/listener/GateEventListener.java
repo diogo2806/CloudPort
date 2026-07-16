5a55ee9470fafbe8c50c811aada8f2aa69cb570c
Diogo Santos <diogo_2806@hotmail.com>
2026-07-16 17:52:59 -0300

    feat: tornar consumidor do Gate idempotente

--- backend/servico-visibilidade/src/main/java/br/com/cloudport/visibilidade/listener/GateEventListener.java
+++ backend/servico-visibilidade/src/main/java/br/com/cloudport/visibilidade/listener/GateEventListener.java
@@ -2,7 +2,9 @@ package br.com.cloudport.visibilidade.listener;
 
 import br.com.cloudport.visibilidade.config.RabbitMQConfig;
 import br.com.cloudport.visibilidade.service.MovimentoConteinerService;
+import br.com.cloudport.visibilidade.service.ProcessamentoEventoIdempotenteService;
 import java.util.Map;
+import java.util.function.Consumer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
@@ -15,9 +17,12 @@ public class GateEventListener {
     private static final Logger LOGGER = LoggerFactory.getLogger(GateEventListener.class);
 
     private final MovimentoConteinerService movimentoConteinerService;
+    private final ProcessamentoEventoIdempotenteService processamentoEventoIdempotenteService;
 
-    public GateEventListener(MovimentoConteinerService movimentoConteinerService) {
+    public GateEventListener(MovimentoConteinerService movimentoConteinerService,
+                             ProcessamentoEventoIdempotenteService processamentoEventoIdempotenteService) {
         this.movimentoConteinerService = movimentoConteinerService;
+        this.processamentoEventoIdempotenteService = processamentoEventoIdempotenteService;
     }
 
     @RabbitListener(queues = RabbitMQConfig.VISIBILIDADE_GATE_QUEUE)
@@ -38,16 +43,30 @@ public class GateEventListener {
 
         switch (eventType) {
             case "gate.container.entered":
-                movimentoConteinerService.registrarEntradaGate(containerId, responsavel);
-                LOGGER.info("Entrada de conteiner registrada. containerId={}", containerId);
+                processarUmaVez(event, identidadeEvento -> {
+                    movimentoConteinerService.registrarEntradaGate(
+                            identidadeEvento, containerId, responsavel);
+                    LOGGER.info("Entrada de conteiner registrada. containerId={}", containerId);
+                });
                 break;
             case "gate.container.exited":
-                movimentoConteinerService.registrarSaidaGate(containerId, responsavel);
-                LOGGER.info("Saida de conteiner registrada. containerId={}", containerId);
+                processarUmaVez(event, identidadeEvento -> {
+                    movimentoConteinerService.registrarSaidaGate(
+                            identidadeEvento, containerId, responsavel);
+                    LOGGER.info("Saida de conteiner registrada. containerId={}", containerId);
+                });
                 break;
             default:
                 LOGGER.debug("Evento de gate sem processador registrado. eventType={} containerId={}",
                         eventType, containerId);
+        }
+    }
+
+    private void processarUmaVez(Map<String, Object> event, Consumer<String> processamento) {
+        boolean processado = processamentoEventoIdempotenteService.processarUmaVez(event, processamento);
+        if (!processado) {
+            LOGGER.info("Redelivery de evento de gate ignorada. eventType={} identidade={}",
+                    texto(event, "eventType"), primeiroTexto(event, "eventId", "messageId"));
         }
     }
 

e1fc0b78e78d68d408f64c7b387b7f3195e4370c
Diogo Santos <diogo_2806@hotmail.com>
2026-07-16 09:01:34 -0300

    fix: compartilhar conversor Rabbit entre módulos

--- backend/servico-rail/src/main/java/br/com/cloudport/servicorail/configuracao/RabbitConfiguracao.java
+++ backend/servico-rail/src/main/java/br/com/cloudport/servicorail/configuracao/RabbitConfiguracao.java
@@ -2,6 +2,7 @@ package br.com.cloudport.servicorail.configuracao;
 
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 public class RabbitConfiguracao {
 
     @Bean
+    @ConditionalOnMissingBean(MessageConverter.class)
     public MessageConverter conversorMensagemJson() {
         return new Jackson2JsonMessageConverter();
     }

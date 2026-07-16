3c927534cb26375206c10e84485b2849970cdfb8
Diogo Santos <diogo_2806@hotmail.com>
2026-07-16 18:00:38 -0300

    fix(bus10): compensar também o estado anterior das work queues

--- backend/servico-yard/src/main/java/br/com/cloudport/servicoyard/patio/listatrabalho/dto/CompensacaoPlanoOtimizadoPatioDto.java
+++ backend/servico-yard/src/main/java/br/com/cloudport/servicoyard/patio/listatrabalho/dto/CompensacaoPlanoOtimizadoPatioDto.java
@@ -1,6 +1,7 @@
 package br.com.cloudport.servicoyard.patio.listatrabalho.dto;
 
 import br.com.cloudport.servicoyard.patio.listatrabalho.dto.ResultadoAplicacaoPlanoOtimizadoPatioDto.EstadoAnteriorOrdemDto;
+import br.com.cloudport.servicoyard.patio.listatrabalho.dto.ResultadoAplicacaoPlanoOtimizadoPatioDto.EstadoAnteriorWorkQueueDto;
 import java.util.ArrayList;
 import java.util.List;
 import javax.validation.Valid;
@@ -26,43 +27,21 @@ public class CompensacaoPlanoOtimizadoPatioDto {
     @NotEmpty
     private List<EstadoAnteriorOrdemDto> estadosAnteriores = new ArrayList<>();
 
-    public String getPlanoId() {
-        return planoId;
-    }
+    @Valid
+    private List<EstadoAnteriorWorkQueueDto> estadosAnterioresWorkQueues = new ArrayList<>();
 
-    public void setPlanoId(String planoId) {
-        this.planoId = planoId;
-    }
-
-    public Long getVisitaNavioId() {
-        return visitaNavioId;
-    }
-
-    public void setVisitaNavioId(Long visitaNavioId) {
-        this.visitaNavioId = visitaNavioId;
-    }
-
-    public String getUsuario() {
-        return usuario;
-    }
-
-    public void setUsuario(String usuario) {
-        this.usuario = usuario;
-    }
-
-    public String getMotivo() {
-        return motivo;
-    }
-
-    public void setMotivo(String motivo) {
-        this.motivo = motivo;
-    }
-
-    public List<EstadoAnteriorOrdemDto> getEstadosAnteriores() {
-        return estadosAnteriores;
-    }
-
-    public void setEstadosAnteriores(List<EstadoAnteriorOrdemDto> estadosAnteriores) {
-        this.estadosAnteriores = estadosAnteriores;
+    public String getPlanoId() { return planoId; }
+    public void setPlanoId(String planoId) { this.planoId = planoId; }
+    public Long getVisitaNavioId() { return visitaNavioId; }
+    public void setVisitaNavioId(Long visitaNavioId) { this.visitaNavioId = visitaNavioId; }
+    public String getUsuario() { return usuario; }
+    public void setUsuario(String usuario) { this.usuario = usuario; }
+    public String getMotivo() { return motivo; }
+    public void setMotivo(String motivo) { this.motivo = motivo; }
+    public List<EstadoAnteriorOrdemDto> getEstadosAnteriores() { return estadosAnteriores; }
+    public void setEstadosAnteriores(List<EstadoAnteriorOrdemDto> estadosAnteriores) { this.estadosAnteriores = estadosAnteriores; }
+    public List<EstadoAnteriorWorkQueueDto> getEstadosAnterioresWorkQueues() { return estadosAnterioresWorkQueues; }
+    public void setEstadosAnterioresWorkQueues(List<EstadoAnteriorWorkQueueDto> estadosAnterioresWorkQueues) {
+        this.estadosAnterioresWorkQueues = estadosAnterioresWorkQueues;
     }
 }

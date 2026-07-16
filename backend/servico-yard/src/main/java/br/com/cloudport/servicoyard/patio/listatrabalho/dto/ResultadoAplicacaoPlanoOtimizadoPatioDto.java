5d6d8b3ccf9f74b9262eae6b491aad1993f919b2
Diogo Santos <diogo_2806@hotmail.com>
2026-07-16 18:00:16 -0300

    fix(bus10): incluir estado anterior das work queues na compensação

--- backend/servico-yard/src/main/java/br/com/cloudport/servicoyard/patio/listatrabalho/dto/ResultadoAplicacaoPlanoOtimizadoPatioDto.java
+++ backend/servico-yard/src/main/java/br/com/cloudport/servicoyard/patio/listatrabalho/dto/ResultadoAplicacaoPlanoOtimizadoPatioDto.java
@@ -9,6 +9,7 @@ public class ResultadoAplicacaoPlanoOtimizadoPatioDto {
     private Long visitaNavioId;
     private int ordensAtualizadas;
     private List<EstadoAnteriorOrdemDto> estadosAnteriores = new ArrayList<>();
+    private List<EstadoAnteriorWorkQueueDto> estadosAnterioresWorkQueues = new ArrayList<>();
 
     public String getPlanoId() {
         return planoId;
@@ -42,6 +43,16 @@ public class ResultadoAplicacaoPlanoOtimizadoPatioDto {
         this.estadosAnteriores = estadosAnteriores;
     }
 
+    public List<EstadoAnteriorWorkQueueDto> getEstadosAnterioresWorkQueues() {
+        return estadosAnterioresWorkQueues;
+    }
+
+    public void setEstadosAnterioresWorkQueues(
+            List<EstadoAnteriorWorkQueueDto> estadosAnterioresWorkQueues
+    ) {
+        this.estadosAnterioresWorkQueues = estadosAnterioresWorkQueues;
+    }
+
     public static class EstadoAnteriorOrdemDto {
 
         private Long ordemTrabalhoPatioId;
@@ -53,68 +64,35 @@ public class ResultadoAplicacaoPlanoOtimizadoPatioDto {
         private Integer sequenciaNavio;
         private Long workQueueId;
 
-        public Long getOrdemTrabalhoPatioId() {
-            return ordemTrabalhoPatioId;
-        }
+        public Long getOrdemTrabalhoPatioId() { return ordemTrabalhoPatioId; }
+        public void setOrdemTrabalhoPatioId(Long ordemTrabalhoPatioId) { this.ordemTrabalhoPatioId = ordemTrabalhoPatioId; }
+        public String getDestino() { return destino; }
+        public void setDestino(String destino) { this.destino = destino; }
+        public Integer getLinhaDestino() { return linhaDestino; }
+        public void setLinhaDestino(Integer linhaDestino) { this.linhaDestino = linhaDestino; }
+        public Integer getColunaDestino() { return colunaDestino; }
+        public void setColunaDestino(Integer colunaDestino) { this.colunaDestino = colunaDestino; }
+        public String getCamadaDestino() { return camadaDestino; }
+        public void setCamadaDestino(String camadaDestino) { this.camadaDestino = camadaDestino; }
+        public Integer getPrioridadeOperacional() { return prioridadeOperacional; }
+        public void setPrioridadeOperacional(Integer prioridadeOperacional) { this.prioridadeOperacional = prioridadeOperacional; }
+        public Integer getSequenciaNavio() { return sequenciaNavio; }
+        public void setSequenciaNavio(Integer sequenciaNavio) { this.sequenciaNavio = sequenciaNavio; }
+        public Long getWorkQueueId() { return workQueueId; }
+        public void setWorkQueueId(Long workQueueId) { this.workQueueId = workQueueId; }
+    }
 
-        public void setOrdemTrabalhoPatioId(Long ordemTrabalhoPatioId) {
-            this.ordemTrabalhoPatioId = ordemTrabalhoPatioId;
-        }
+    public static class EstadoAnteriorWorkQueueDto {
 
-        public String getDestino() {
-            return destino;
-        }
+        private Long workQueueId;
+        private Integer sequenciaInicial;
+        private Integer prioridadeOperacional;
 
-        public void setDestino(String destino) {
-            this.destino = destino;
-        }
-
-        public Integer getLinhaDestino() {
-            return linhaDestino;
-        }
-
-        public void setLinhaDestino(Integer linhaDestino) {
-            this.linhaDestino = linhaDestino;
-        }
-
-        public Integer getColunaDestino() {
-            return colunaDestino;
-        }
-
-        public void setColunaDestino(Integer colunaDestino) {
-            this.colunaDestino = colunaDestino;
-        }
-
-        public String getCamadaDestino() {
-            return camadaDestino;
-        }
-
-        public void setCamadaDestino(String camadaDestino) {
-            this.camadaDestino = camadaDestino;
-        }
-
-        public Integer getPrioridadeOperacional() {
-            return prioridadeOperacional;
-        }
-
-        public void setPrioridadeOperacional(Integer prioridadeOperacional) {
-            this.prioridadeOperacional = prioridadeOperacional;
-        }
-
-        public Integer getSequenciaNavio() {
-            return sequenciaNavio;
-        }
-
-        public void setSequenciaNavio(Integer sequenciaNavio) {
-            this.sequenciaNavio = sequenciaNavio;
-        }
-
-        public Long getWorkQueueId() {
-            return workQueueId;
-        }
-
-        public void setWorkQueueId(Long workQueueId) {
-            this.workQueueId = workQueueId;
-        }
+        public Long getWorkQueueId() { return workQueueId; }
+        public void setWorkQueueId(Long workQueueId) { this.workQueueId = workQueueId; }
+        public Integer getSequenciaInicial() { return sequenciaInicial; }
+        public void setSequenciaInicial(Integer sequenciaInicial) { this.sequenciaInicial = sequenciaInicial; }
+        public Integer getPrioridadeOperacional() { return prioridadeOperacional; }
+        public void setPrioridadeOperacional(Integer prioridadeOperacional) { this.prioridadeOperacional = prioridadeOperacional; }
     }
 }

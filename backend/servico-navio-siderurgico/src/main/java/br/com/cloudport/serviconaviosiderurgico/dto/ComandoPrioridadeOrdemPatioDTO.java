a74557d4c0b0ad7641338774a0fc29a7a28cc532
Diogo Santos <diogo_2806@hotmail.com>
2026-07-16 09:23:47 -0300

    feat: exigir motivo na alteracao de prioridade integrada

--- backend/servico-navio-siderurgico/src/main/java/br/com/cloudport/serviconaviosiderurgico/dto/ComandoPrioridadeOrdemPatioDTO.java
+++ backend/servico-navio-siderurgico/src/main/java/br/com/cloudport/serviconaviosiderurgico/dto/ComandoPrioridadeOrdemPatioDTO.java
@@ -1,12 +1,17 @@
 package br.com.cloudport.serviconaviosiderurgico.dto;
 
 import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 public record ComandoPrioridadeOrdemPatioDTO(
         @NotNull @Min(0) Integer prioridadeOperacional,
         Boolean prioridadeBusca,
-        String usuario
+        @NotBlank @Size(max = 500) String motivo,
+        String usuario,
+        String origemAcao,
+        String correlationId
 ) {
     public boolean prioridadeBuscaEfetiva() {
         return Boolean.TRUE.equals(prioridadeBusca);

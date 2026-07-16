33c0a2e949f3ad1c894a18f186a6357edd1ba1bc
Diogo Santos <diogo_2806@hotmail.com>
2026-07-16 09:09:41 -0300

    feat: padronizar enums externos

---
+++ backend/cloudport-contracts/src/main/java/br/com/cloudport/contracts/enums/StatusVisitaContrato.java
@@ -0,0 +1,11 @@
+package br.com.cloudport.contracts.enums;
+
+public enum StatusVisitaContrato {
+    PREVISTA,
+    FUNDEADA,
+    ATRACADA,
+    OPERANDO,
+    OPERACAO_CONCLUIDA,
+    PARTIU,
+    CANCELADA
+}

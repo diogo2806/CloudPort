2f2dfb99536adbb31effbe94a3a336705383c2ed
Diogo Santos <diogo_2806@hotmail.com>
2026-07-16 09:09:59 -0300

    feat: padronizar status de ordem

---
+++ backend/cloudport-contracts/src/main/java/br/com/cloudport/contracts/enums/StatusOrdemContrato.java
@@ -0,0 +1,11 @@
+package br.com.cloudport.contracts.enums;
+
+public enum StatusOrdemContrato {
+    PENDENTE,
+    EM_EXECUCAO,
+    SUSPENSA,
+    CONCLUIDA,
+    CANCELADA,
+    BLOQUEADA,
+    REJEITADA
+}

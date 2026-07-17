90f49afaec67be55bd003faa00293b5801370422
Diogo Santos <diogo_2806@hotmail.com>
2026-07-17 19:09:54 -0300

    feat(rail): modelar ciclo intermodal da locomotiva

---
+++ backend/servico-rail/src/main/java/br/com/cloudport/servicorail/ferrovia/locomotiva/modelo/StatusTransferenciaLocomotiva.java
@@ -0,0 +1,10 @@
+package br.com.cloudport.servicorail.ferrovia.locomotiva.modelo;
+
+public enum StatusTransferenciaLocomotiva {
+    AGUARDANDO_ENTREGA,
+    SOB_CUSTODIA_TERMINAL,
+    PLANEJADA_PARA_EMBARQUE,
+    PRONTA_PARA_EMBARQUE,
+    EMBARCADA,
+    CANCELADA
+}

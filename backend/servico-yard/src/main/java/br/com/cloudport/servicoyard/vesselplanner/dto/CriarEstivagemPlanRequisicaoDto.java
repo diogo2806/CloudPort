cfee6c305d4c5be692eb0243c69d8e2b7b6b9e0d
Diogo Santos <diogo_2806@hotmail.com>
2026-07-17 14:59:02 -0300

    feat(arch20): exigir visita canônica na criação do vessel planner

---
+++ backend/servico-yard/src/main/java/br/com/cloudport/servicoyard/vesselplanner/dto/CriarEstivagemPlanRequisicaoDto.java
@@ -0,0 +1,28 @@
+package br.com.cloudport.servicoyard.vesselplanner.dto;
+
+import javax.validation.constraints.NotNull;
+
+public class CriarEstivagemPlanRequisicaoDto {
+
+    @NotNull
+    private Long bayPlanId;
+
+    @NotNull
+    private Long visitaNavioId;
+
+    public Long getBayPlanId() {
+        return bayPlanId;
+    }
+
+    public void setBayPlanId(Long bayPlanId) {
+        this.bayPlanId = bayPlanId;
+    }
+
+    public Long getVisitaNavioId() {
+        return visitaNavioId;
+    }
+
+    public void setVisitaNavioId(Long visitaNavioId) {
+        this.visitaNavioId = visitaNavioId;
+    }
+}

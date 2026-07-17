3192d0e2610e5ea79dad58262f8b4d0922fb7bc6
Diogo Santos <diogo_2806@hotmail.com>
2026-07-17 14:51:53 -0300

    feat(yard): exigir dados reais de dunnage e lashing

--- backend/servico-yard/src/main/java/br/com/cloudport/servicoyard/estivagembulk/dto/PosicionarBobinaRequisicaoDto.java
+++ backend/servico-yard/src/main/java/br/com/cloudport/servicoyard/estivagembulk/dto/PosicionarBobinaRequisicaoDto.java
@@ -1,6 +1,8 @@
 package br.com.cloudport.servicoyard.estivagembulk.dto;
 
 import br.com.cloudport.servicoyard.estivagembulk.modelo.TipoLashing;
+import java.util.ArrayList;
+import java.util.List;
 
 public class PosicionarBobinaRequisicaoDto {
 
@@ -8,10 +10,22 @@ public class PosicionarBobinaRequisicaoDto {
     private Long poraoId;
     private Long setorId;
     private int camada;
-    private double posicaoX;
-    private double posicaoY;
-    private double espessuraDunnageMm;
+    private Double posicaoX;
+    private Double posicaoY;
+    private Double anguloInclinacao;
+    private Double espessuraDunnageMm;
+    private Integer quantidadeLinhasDunnage;
+    private Double larguraDunnageMm;
+    private Double comprimentoContatoDunnageMm;
+    private Integer quantidadeCalcos;
+    private Double espacamentoFileirasMm;
     private TipoLashing tipoLashing;
+    private Double forcaRequeridaLashingKn;
+    private Integer sequenciaDescarga;
+    private String referenciaRegra;
+    private String versaoEspecificacao;
+    private String responsavelValidacao;
+    private List<MaterialLashingDto> materiaisLashing = new ArrayList<>();
 
     public PosicionarBobinaRequisicaoDto() {
     }
@@ -60,28 +74,76 @@ public class PosicionarBobinaRequisicaoDto {
         this.camada = camada;
     }
 
-    public double getPosicaoX() {
+    public Double getPosicaoX() {
         return posicaoX;
     }
 
-    public void setPosicaoX(double posicaoX) {
+    public void setPosicaoX(Double posicaoX) {
         this.posicaoX = posicaoX;
     }
 
-    public double getPosicaoY() {
+    public Double getPosicaoY() {
         return posicaoY;
     }
 
-    public void setPosicaoY(double posicaoY) {
+    public void setPosicaoY(Double posicaoY) {
         this.posicaoY = posicaoY;
     }
 
-    public double getEspessuraDunnageMm() {
+    public Double getAnguloInclinacao() {
+        return anguloInclinacao;
+    }
+
+    public void setAnguloInclinacao(Double anguloInclinacao) {
+        this.anguloInclinacao = anguloInclinacao;
+    }
+
+    public Double getEspessuraDunnageMm() {
         return espessuraDunnageMm;
     }
 
-    public void setEspessuraDunnageMm(double espessuraDunnageMm) {
+    public void setEspessuraDunnageMm(Double espessuraDunnageMm) {
         this.espessuraDunnageMm = espessuraDunnageMm;
+    }
+
+    public Integer getQuantidadeLinhasDunnage() {
+        return quantidadeLinhasDunnage;
+    }
+
+    public void setQuantidadeLinhasDunnage(Integer quantidadeLinhasDunnage) {
+        this.quantidadeLinhasDunnage = quantidadeLinhasDunnage;
+    }
+
+    public Double getLarguraDunnageMm() {
+        return larguraDunnageMm;
+    }
+
+    public void setLarguraDunnageMm(Double larguraDunnageMm) {
+        this.larguraDunnageMm = larguraDunnageMm;
+    }
+
+    public Double getComprimentoContatoDunnageMm() {
+        return comprimentoContatoDunnageMm;
+    }
+
+    public void setComprimentoContatoDunnageMm(Double comprimentoContatoDunnageMm) {
+        this.comprimentoContatoDunnageMm = comprimentoContatoDunnageMm;
+    }
+
+    public Integer getQuantidadeCalcos() {
+        return quantidadeCalcos;
+    }
+
+    public void setQuantidadeCalcos(Integer quantidadeCalcos) {
+        this.quantidadeCalcos = quantidadeCalcos;
+    }
+
+    public Double getEspacamentoFileirasMm() {
+        return espacamentoFileirasMm;
+    }
+
+    public void setEspacamentoFileirasMm(Double espacamentoFileirasMm) {
+        this.espacamentoFileirasMm = espacamentoFileirasMm;
     }
 
     public TipoLashing getTipoLashing() {
@@ -90,5 +152,53 @@ public class PosicionarBobinaRequisicaoDto {
 
     public void setTipoLashing(TipoLashing tipoLashing) {
         this.tipoLashing = tipoLashing;
+    }
+
+    public Double getForcaRequeridaLashingKn() {
+        return forcaRequeridaLashingKn;
+    }
+
+    public void setForcaRequeridaLashingKn(Double forcaRequeridaLashingKn) {
+        this.forcaRequeridaLashingKn = forcaRequeridaLashingKn;
+    }
+
+    public Integer getSequenciaDescarga() {
+        return sequenciaDescarga;
+    }
+
+    public void setSequenciaDescarga(Integer sequenciaDescarga) {
+        this.sequenciaDescarga = sequenciaDescarga;
+    }
+
+    public String getReferenciaRegra() {
+        return referenciaRegra;
+    }
+
+    public void setReferenciaRegra(String referenciaRegra) {
+        this.referenciaRegra = referenciaRegra;
+    }
+
+    public String getVersaoEspecificacao() {
+        return versaoEspecificacao;
+    }
+
+    public void setVersaoEspecificacao(String versaoEspecificacao) {
+        this.versaoEspecificacao = versaoEspecificacao;
+    }
+
+    public String getResponsavelValidacao() {
+        return responsavelValidacao;
+    }
+
+    public void setResponsavelValidacao(String responsavelValidacao) {
+        this.responsavelValidacao = responsavelValidacao;
+    }
+
+    public List<MaterialLashingDto> getMateriaisLashing() {
+        return materiaisLashing;
+    }
+
+    public void setMateriaisLashing(List<MaterialLashingDto> materiaisLashing) {
+        this.materiaisLashing = materiaisLashing;
     }
 }

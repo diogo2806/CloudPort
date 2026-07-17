51eaa22b55b69e7ba51e48c7afe857404906c277
Diogo Santos <diogo_2806@hotmail.com>
2026-07-17 07:22:38 -0300

    feat(gate): auditar entrega de alertas de reconciliação

--- backend/servico-gate/src/main/java/br/com/cloudport/servicogate/model/ReconciliacaoBarcode.java
+++ backend/servico-gate/src/main/java/br/com/cloudport/servicogate/model/ReconciliacaoBarcode.java
@@ -1,5 +1,6 @@
 package br.com.cloudport.servicogate.model;
 
+import br.com.cloudport.servicogate.model.enums.StatusEntregaAlerta;
 import br.com.cloudport.servicogate.model.enums.TipoDesincroniaBarcode;
 import java.time.LocalDateTime;
 import javax.persistence.Column;
@@ -58,109 +59,63 @@ public class ReconciliacaoBarcode extends AbstractAuditableEntity {
     private String resolucao;
 
     @Column(name = "alerta_enviado", nullable = false)
-    private boolean alertaEnviado = false;
+    private boolean alertaEnviado;
 
-    public Long getId() {
-        return id;
-    }
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status_entrega_alerta", nullable = false, length = 20)
+    private StatusEntregaAlerta statusEntregaAlerta = StatusEntregaAlerta.PENDENTE;
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+    @Column(name = "alerta_enviado_em")
+    private LocalDateTime alertaEnviadoEm;
 
-    public GatePass getGatePass() {
-        return gatePass;
-    }
+    @Column(name = "alerta_canal", length = 40)
+    private String alertaCanal;
 
-    public void setGatePass(GatePass gatePass) {
-        this.gatePass = gatePass;
-    }
+    @Column(name = "alerta_identificador_externo", length = 120)
+    private String alertaIdentificadorExterno;
 
-    public TipoDesincroniaBarcode getTipoDesinconia() {
-        return tipoDesinconia;
-    }
+    @Column(name = "alerta_tentativas", nullable = false)
+    private Integer alertaTentativas = 0;
 
-    public void setTipoDesinconia(TipoDesincroniaBarcode tipoDesinconia) {
-        this.tipoDesinconia = tipoDesinconia;
-    }
+    @Column(name = "alerta_ultimo_erro", length = 1000)
+    private String alertaUltimoErro;
 
-    public String getDescricao() {
-        return descricao;
-    }
-
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
-
-    public String getBarcodeEsperado() {
-        return barcodeEsperado;
-    }
-
-    public void setBarcodeEsperado(String barcodeEsperado) {
-        this.barcodeEsperado = barcodeEsperado;
-    }
-
-    public String getBarcodeRecebido() {
-        return barcodeRecebido;
-    }
-
-    public void setBarcodeRecebido(String barcodeRecebido) {
-        this.barcodeRecebido = barcodeRecebido;
-    }
-
-    public String getStatusTos() {
-        return statusTos;
-    }
-
-    public void setStatusTos(String statusTos) {
-        this.statusTos = statusTos;
-    }
-
-    public String getStatusLocal() {
-        return statusLocal;
-    }
-
-    public void setStatusLocal(String statusLocal) {
-        this.statusLocal = statusLocal;
-    }
-
-    public Integer getTempoPendenciaHoras() {
-        return tempoPendenciaHoras;
-    }
-
-    public void setTempoPendenciaHoras(Integer tempoPendenciaHoras) {
-        this.tempoPendenciaHoras = tempoPendenciaHoras;
-    }
-
-    public LocalDateTime getDetectadoEm() {
-        return detectadoEm;
-    }
-
-    public void setDetectadoEm(LocalDateTime detectadoEm) {
-        this.detectadoEm = detectadoEm;
-    }
-
-    public LocalDateTime getResolvidoEm() {
-        return resolvidoEm;
-    }
-
-    public void setResolvidoEm(LocalDateTime resolvidoEm) {
-        this.resolvidoEm = resolvidoEm;
-    }
-
-    public String getResolucao() {
-        return resolucao;
-    }
-
-    public void setResolucao(String resolucao) {
-        this.resolucao = resolucao;
-    }
-
-    public boolean isAlertaEnviado() {
-        return alertaEnviado;
-    }
-
-    public void setAlertaEnviado(boolean alertaEnviado) {
-        this.alertaEnviado = alertaEnviado;
-    }
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+    public GatePass getGatePass() { return gatePass; }
+    public void setGatePass(GatePass gatePass) { this.gatePass = gatePass; }
+    public TipoDesincroniaBarcode getTipoDesinconia() { return tipoDesinconia; }
+    public void setTipoDesinconia(TipoDesincroniaBarcode tipoDesinconia) { this.tipoDesinconia = tipoDesinconia; }
+    public String getDescricao() { return descricao; }
+    public void setDescricao(String descricao) { this.descricao = descricao; }
+    public String getBarcodeEsperado() { return barcodeEsperado; }
+    public void setBarcodeEsperado(String barcodeEsperado) { this.barcodeEsperado = barcodeEsperado; }
+    public String getBarcodeRecebido() { return barcodeRecebido; }
+    public void setBarcodeRecebido(String barcodeRecebido) { this.barcodeRecebido = barcodeRecebido; }
+    public String getStatusTos() { return statusTos; }
+    public void setStatusTos(String statusTos) { this.statusTos = statusTos; }
+    public String getStatusLocal() { return statusLocal; }
+    public void setStatusLocal(String statusLocal) { this.statusLocal = statusLocal; }
+    public Integer getTempoPendenciaHoras() { return tempoPendenciaHoras; }
+    public void setTempoPendenciaHoras(Integer tempoPendenciaHoras) { this.tempoPendenciaHoras = tempoPendenciaHoras; }
+    public LocalDateTime getDetectadoEm() { return detectadoEm; }
+    public void setDetectadoEm(LocalDateTime detectadoEm) { this.detectadoEm = detectadoEm; }
+    public LocalDateTime getResolvidoEm() { return resolvidoEm; }
+    public void setResolvidoEm(LocalDateTime resolvidoEm) { this.resolvidoEm = resolvidoEm; }
+    public String getResolucao() { return resolucao; }
+    public void setResolucao(String resolucao) { this.resolucao = resolucao; }
+    public boolean isAlertaEnviado() { return alertaEnviado; }
+    public void setAlertaEnviado(boolean alertaEnviado) { this.alertaEnviado = alertaEnviado; }
+    public StatusEntregaAlerta getStatusEntregaAlerta() { return statusEntregaAlerta; }
+    public void setStatusEntregaAlerta(StatusEntregaAlerta statusEntregaAlerta) { this.statusEntregaAlerta = statusEntregaAlerta; }
+    public LocalDateTime getAlertaEnviadoEm() { return alertaEnviadoEm; }
+    public void setAlertaEnviadoEm(LocalDateTime alertaEnviadoEm) { this.alertaEnviadoEm = alertaEnviadoEm; }
+    public String getAlertaCanal() { return alertaCanal; }
+    public void setAlertaCanal(String alertaCanal) { this.alertaCanal = alertaCanal; }
+    public String getAlertaIdentificadorExterno() { return alertaIdentificadorExterno; }
+    public void setAlertaIdentificadorExterno(String alertaIdentificadorExterno) { this.alertaIdentificadorExterno = alertaIdentificadorExterno; }
+    public Integer getAlertaTentativas() { return alertaTentativas; }
+    public void setAlertaTentativas(Integer alertaTentativas) { this.alertaTentativas = alertaTentativas; }
+    public String getAlertaUltimoErro() { return alertaUltimoErro; }
+    public void setAlertaUltimoErro(String alertaUltimoErro) { this.alertaUltimoErro = alertaUltimoErro; }
 }

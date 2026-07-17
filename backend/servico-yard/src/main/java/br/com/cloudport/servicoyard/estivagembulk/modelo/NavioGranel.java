4ca13b5dc346d2819a6d67aac23e785ac5f66400
Diogo Santos <diogo_2806@hotmail.com>
2026-07-17 16:35:56 -0300

    fix(pr-318): preservar alterações da main em NavioGranel

--- backend/servico-yard/src/main/java/br/com/cloudport/servicoyard/estivagembulk/modelo/NavioGranel.java
+++ backend/servico-yard/src/main/java/br/com/cloudport/servicoyard/estivagembulk/modelo/NavioGranel.java
@@ -16,11 +16,7 @@ import javax.persistence.OneToMany;
 import javax.persistence.PrePersist;
 import javax.persistence.PreUpdate;
 import javax.persistence.Table;
-import lombok.Getter;
-import lombok.Setter;
 
-@Getter
-@Setter
 @Entity
 @Table(name = "navio_granel")
 public class NavioGranel {
@@ -143,4 +139,87 @@ public class NavioGranel {
         }
         atualizadoEm = now;
     }
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+    public Long getNavioCadastroId() { return navioCadastroId; }
+    public void setNavioCadastroId(Long navioCadastroId) { this.navioCadastroId = navioCadastroId; }
+    public Long getVersaoPerfil() { return versaoPerfil; }
+    public void setVersaoPerfil(Long versaoPerfil) { this.versaoPerfil = versaoPerfil; }
+    public Long getVersaoNavioCanonico() { return versaoNavioCanonico; }
+    public void setVersaoNavioCanonico(Long versaoNavioCanonico) { this.versaoNavioCanonico = versaoNavioCanonico; }
+    public String getImo() { return imo; }
+    public void setImo(String imo) { this.imo = imo; }
+    public String getNome() { return nome; }
+    public void setNome(String nome) { this.nome = nome; }
+    public ClasseNavio getClasse() { return classe; }
+    public void setClasse(ClasseNavio classe) { this.classe = classe; }
+    public Double getLpp() { return lpp; }
+    public void setLpp(Double lpp) { this.lpp = lpp; }
+    public Double getBoca() { return boca; }
+    public void setBoca(Double boca) { this.boca = boca; }
+    public Double getCalado() { return calado; }
+    public void setCalado(Double calado) { this.calado = calado; }
+    public Double getDeslocamento() { return deslocamento; }
+    public void setDeslocamento(Double deslocamento) { this.deslocamento = deslocamento; }
+    public Double getGm() { return gm; }
+    public void setGm(Double gm) { this.gm = gm; }
+    public Double getTpc() { return tpc; }
+    public void setTpc(Double tpc) { this.tpc = tpc; }
+    public Double getLcb() { return lcb; }
+    public void setLcb(Double lcb) { this.lcb = lcb; }
+    public Double getKm() { return km; }
+    public void setKm(Double km) { this.km = km; }
+    public Double getMct1cm() { return mct1cm; }
+    public void setMct1cm(Double mct1cm) { this.mct1cm = mct1cm; }
+    public Double getCaladoMaximo() { return caladoMaximo; }
+    public void setCaladoMaximo(Double caladoMaximo) { this.caladoMaximo = caladoMaximo; }
+    public Double getTrimMaximo() { return trimMaximo; }
+    public void setTrimMaximo(Double trimMaximo) { this.trimMaximo = trimMaximo; }
+    public Double getBandaMaxima() { return bandaMaxima; }
+    public void setBandaMaxima(Double bandaMaxima) { this.bandaMaxima = bandaMaxima; }
+    public Double getGmMinimo() { return gmMinimo; }
+    public void setGmMinimo(Double gmMinimo) { this.gmMinimo = gmMinimo; }
+    public Double getPesoLeveToneladas() { return pesoLeveToneladas; }
+    public void setPesoLeveToneladas(Double pesoLeveToneladas) { this.pesoLeveToneladas = pesoLeveToneladas; }
+    public Double getLcgPesoLeve() { return lcgPesoLeve; }
+    public void setLcgPesoLeve(Double lcgPesoLeve) { this.lcgPesoLeve = lcgPesoLeve; }
+    public Double getTcgPesoLeve() { return tcgPesoLeve; }
+    public void setTcgPesoLeve(Double tcgPesoLeve) { this.tcgPesoLeve = tcgPesoLeve; }
+    public Double getVcgPesoLeve() { return vcgPesoLeve; }
+    public void setVcgPesoLeve(Double vcgPesoLeve) { this.vcgPesoLeve = vcgPesoLeve; }
+    public Double getPesoLastroToneladas() { return pesoLastroToneladas; }
+    public void setPesoLastroToneladas(Double pesoLastroToneladas) { this.pesoLastroToneladas = pesoLastroToneladas; }
+    public Double getLcgLastro() { return lcgLastro; }
+    public void setLcgLastro(Double lcgLastro) { this.lcgLastro = lcgLastro; }
+    public Double getTcgLastro() { return tcgLastro; }
+    public void setTcgLastro(Double tcgLastro) { this.tcgLastro = tcgLastro; }
+    public Double getVcgLastro() { return vcgLastro; }
+    public void setVcgLastro(Double vcgLastro) { this.vcgLastro = vcgLastro; }
+    public Double getBmMaxPermitido() { return bmMaxPermitido; }
+    public void setBmMaxPermitido(Double bmMaxPermitido) { this.bmMaxPermitido = bmMaxPermitido; }
+    public Double getSfMaxPermitido() { return sfMaxPermitido; }
+    public void setSfMaxPermitido(Double sfMaxPermitido) { this.sfMaxPermitido = sfMaxPermitido; }
+    public String getVersaoDadosHidrostaticos() { return versaoDadosHidrostaticos; }
+    public void setVersaoDadosHidrostaticos(String versaoDadosHidrostaticos) { this.versaoDadosHidrostaticos = versaoDadosHidrostaticos; }
+    public String getVersaoDadosEstruturais() { return versaoDadosEstruturais; }
+    public void setVersaoDadosEstruturais(String versaoDadosEstruturais) { this.versaoDadosEstruturais = versaoDadosEstruturais; }
+    public String getPosicoesSecoes() { return posicoesSecoes; }
+    public void setPosicoesSecoes(String posicoesSecoes) { this.posicoesSecoes = posicoesSecoes; }
+    public String getPesoLeveSecoes() { return pesoLeveSecoes; }
+    public void setPesoLeveSecoes(String pesoLeveSecoes) { this.pesoLeveSecoes = pesoLeveSecoes; }
+    public String getEmpuxoSecoes() { return empuxoSecoes; }
+    public void setEmpuxoSecoes(String empuxoSecoes) { this.empuxoSecoes = empuxoSecoes; }
+    public String getLimitesSfSecoes() { return limitesSfSecoes; }
+    public void setLimitesSfSecoes(String limitesSfSecoes) { this.limitesSfSecoes = limitesSfSecoes; }
+    public String getLimitesBmSecoes() { return limitesBmSecoes; }
+    public void setLimitesBmSecoes(String limitesBmSecoes) { this.limitesBmSecoes = limitesBmSecoes; }
+    public boolean isTemplate() { return isTemplate; }
+    public void setTemplate(boolean template) { isTemplate = template; }
+    public List<PoraoNavio> getPoroes() { return poroes; }
+    public void setPoroes(List<PoraoNavio> poroes) { this.poroes = poroes; }
+    public LocalDateTime getCriadoEm() { return criadoEm; }
+    public void setCriadoEm(LocalDateTime criadoEm) { this.criadoEm = criadoEm; }
+    public LocalDateTime getAtualizadoEm() { return atualizadoEm; }
+    public void setAtualizadoEm(LocalDateTime atualizadoEm) { this.atualizadoEm = atualizadoEm; }
 }

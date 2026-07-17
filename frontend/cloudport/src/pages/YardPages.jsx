430e6cedca2c15668eea5978a40381a432d4ae1d
Diogo Santos <diogo_2806@hotmail.com>
2026-07-17 19:14:29 -0300

    feat(front): exportar planejamento de recebimento

--- frontend/cloudport/src/pages/YardPages.jsx
+++ frontend/cloudport/src/pages/YardPages.jsx
@@ -1,3 +1,4 @@
 export { YardMapPage, YardPositionsPage } from './yard/YardMapPages.jsx';
+export { YardReceivingPlanPage } from './yard/YardReceivingPlanPage.jsx';
 export { YardMovementsPage, YardResourcesPage, YardWorkListPage } from './yard/YardWorkPages.jsx';
 export { YardAutomationPage, YardKpiPage } from './yard/YardInsightPages.jsx';

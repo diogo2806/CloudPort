d46e41e1b03cf4c6aa66e37345356a20722495f1
Diogo Santos <diogo_2806@hotmail.com>
2026-07-17 15:21:42 -0300

    feat(arch20): versionar cadastro canônico de navios

--- backend/servico-navio/src/main/java/br/com/cloudport/serviconavio/navio/entidade/Navio.java
+++ backend/servico-navio/src/main/java/br/com/cloudport/serviconavio/navio/entidade/Navio.java
@@ -1,12 +1,13 @@
 package br.com.cloudport.serviconavio.navio.entidade;
 
+import java.math.BigDecimal;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
-import java.math.BigDecimal;
+import javax.persistence.Version;
 
 @Entity
 @Table(name = "navio")
@@ -41,75 +42,28 @@ public class Navio {
     @Column(name = "call_sign", length = 15)
     private String callSign;
 
-    public Long getIdentificador() {
-        return identificador;
-    }
+    @Version
+    @Column(name = "versao", nullable = false)
+    private Long versao;
 
-    public void setIdentificador(Long identificador) {
-        this.identificador = identificador;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getCodigoImo() {
-        return codigoImo;
-    }
-
-    public void setCodigoImo(String codigoImo) {
-        this.codigoImo = codigoImo;
-    }
-
-    public String getPaisBandeira() {
-        return paisBandeira;
-    }
-
-    public void setPaisBandeira(String paisBandeira) {
-        this.paisBandeira = paisBandeira;
-    }
-
-    public String getEmpresaArmadora() {
-        return empresaArmadora;
-    }
-
-    public void setEmpresaArmadora(String empresaArmadora) {
-        this.empresaArmadora = empresaArmadora;
-    }
-
-    public Integer getCapacidadeTeu() {
-        return capacidadeTeu;
-    }
-
-    public void setCapacidadeTeu(Integer capacidadeTeu) {
-        this.capacidadeTeu = capacidadeTeu;
-    }
-
-    public BigDecimal getLoaMetros() {
-        return loaMetros;
-    }
-
-    public void setLoaMetros(BigDecimal loaMetros) {
-        this.loaMetros = loaMetros;
-    }
-
-    public BigDecimal getCaladoMaximoMetros() {
-        return caladoMaximoMetros;
-    }
-
-    public void setCaladoMaximoMetros(BigDecimal caladoMaximoMetros) {
-        this.caladoMaximoMetros = caladoMaximoMetros;
-    }
-
-    public String getCallSign() {
-        return callSign;
-    }
-
-    public void setCallSign(String callSign) {
-        this.callSign = callSign;
-    }
+    public Long getIdentificador() { return identificador; }
+    public void setIdentificador(Long identificador) { this.identificador = identificador; }
+    public String getNome() { return nome; }
+    public void setNome(String nome) { this.nome = nome; }
+    public String getCodigoImo() { return codigoImo; }
+    public void setCodigoImo(String codigoImo) { this.codigoImo = codigoImo; }
+    public String getPaisBandeira() { return paisBandeira; }
+    public void setPaisBandeira(String paisBandeira) { this.paisBandeira = paisBandeira; }
+    public String getEmpresaArmadora() { return empresaArmadora; }
+    public void setEmpresaArmadora(String empresaArmadora) { this.empresaArmadora = empresaArmadora; }
+    public Integer getCapacidadeTeu() { return capacidadeTeu; }
+    public void setCapacidadeTeu(Integer capacidadeTeu) { this.capacidadeTeu = capacidadeTeu; }
+    public BigDecimal getLoaMetros() { return loaMetros; }
+    public void setLoaMetros(BigDecimal loaMetros) { this.loaMetros = loaMetros; }
+    public BigDecimal getCaladoMaximoMetros() { return caladoMaximoMetros; }
+    public void setCaladoMaximoMetros(BigDecimal caladoMaximoMetros) { this.caladoMaximoMetros = caladoMaximoMetros; }
+    public String getCallSign() { return callSign; }
+    public void setCallSign(String callSign) { this.callSign = callSign; }
+    public Long getVersao() { return versao; }
+    public void setVersao(Long versao) { this.versao = versao; }
 }

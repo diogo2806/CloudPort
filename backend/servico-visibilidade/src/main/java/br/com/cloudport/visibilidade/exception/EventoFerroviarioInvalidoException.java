5ff088187565301e457a26ebbf645567beb04c37
Diogo Santos <diogo_2806@hotmail.com>
2026-07-17 07:16:59 -0300

    feat(visibilidade): tipar rejeição ferroviária permanente

---
+++ backend/servico-visibilidade/src/main/java/br/com/cloudport/visibilidade/exception/EventoFerroviarioInvalidoException.java
@@ -0,0 +1,8 @@
+package br.com.cloudport.visibilidade.exception;
+
+public class EventoFerroviarioInvalidoException extends RuntimeException {
+
+    public EventoFerroviarioInvalidoException(String message) {
+        super(message);
+    }
+}

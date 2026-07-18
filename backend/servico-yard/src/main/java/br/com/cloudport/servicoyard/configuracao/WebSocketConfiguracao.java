1cbd29f46071a7efd6058a7f7d4bfe772605e620
Diogo Santos <diogo_2806@hotmail.com>
2026-07-18 06:48:59 -0300

    fix(yard): restringir origens e interceptar WebSocket

--- backend/servico-yard/src/main/java/br/com/cloudport/servicoyard/configuracao/WebSocketConfiguracao.java
+++ backend/servico-yard/src/main/java/br/com/cloudport/servicoyard/configuracao/WebSocketConfiguracao.java
@@ -1,7 +1,11 @@
 package br.com.cloudport.servicoyard.configuracao;
 
+import java.util.Arrays;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.util.StringUtils;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
@@ -10,6 +14,21 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @EnableWebSocketMessageBroker
 public class WebSocketConfiguracao implements WebSocketMessageBrokerConfigurer {
 
+    private static final String ORIGEM_LOCAL_PADRAO = "http://localhost:4200";
+
+    private final String[] origensPermitidas;
+    private final InterceptadorHandshakeWebSocket interceptadorHandshake;
+    private final InterceptadorAutorizacaoWebSocket interceptadorAutorizacao;
+
+    public WebSocketConfiguracao(
+            @Value("${cloudport.security.cors.allowed-origins:http://localhost:4200}") String allowedOrigins,
+            InterceptadorHandshakeWebSocket interceptadorHandshake,
+            InterceptadorAutorizacaoWebSocket interceptadorAutorizacao) {
+        this.origensPermitidas = carregarOrigensPermitidas(allowedOrigins);
+        this.interceptadorHandshake = interceptadorHandshake;
+        this.interceptadorAutorizacao = interceptadorAutorizacao;
+    }
+
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         registry.enableSimpleBroker("/topico");
@@ -17,12 +36,33 @@ public class WebSocketConfiguracao implements WebSocketMessageBrokerConfigurer {
     }
 
     @Override
+    public void configureClientInboundChannel(ChannelRegistration registration) {
+        registration.interceptors(interceptadorAutorizacao);
+    }
+
+    @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws/patio")
-                .setAllowedOriginPatterns("*");
-        registry.addEndpoint("/ws/recursos")
-                .setAllowedOriginPatterns("*");
-        registry.addEndpoint("/ws/edi")
-                .setAllowedOriginPatterns("*");
+        registrarEndpoint(registry, CanalWebSocketOperacional.PATIO);
+        registrarEndpoint(registry, CanalWebSocketOperacional.RECURSOS);
+        registrarEndpoint(registry, CanalWebSocketOperacional.EDI);
+    }
+
+    private void registrarEndpoint(StompEndpointRegistry registry, CanalWebSocketOperacional canal) {
+        registry.addEndpoint(canal.getEndpoint())
+                .setAllowedOrigins(origensPermitidas)
+                .addInterceptors(interceptadorHandshake);
+    }
+
+    private String[] carregarOrigensPermitidas(String allowedOrigins) {
+        String[] origins = Arrays.stream(allowedOrigins.split(","))
+                .map(String::trim)
+                .filter(StringUtils::hasText)
+                .filter(origin -> !"*".equals(origin))
+                .distinct()
+                .toArray(String[]::new);
+        if (origins.length == 0) {
+            return new String[]{ORIGEM_LOCAL_PADRAO};
+        }
+        return origins;
     }
 }

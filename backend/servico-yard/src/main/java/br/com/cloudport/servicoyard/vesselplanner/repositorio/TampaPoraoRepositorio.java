5f6d9dc96a5bc83ab00e3f8095a491ab338f617d
Diogo Santos <diogo_2806@hotmail.com>
2026-07-18 08:32:20 -0300

    feat(vessel-planner): adicionar repositório de tampas

---
+++ backend/servico-yard/src/main/java/br/com/cloudport/servicoyard/vesselplanner/repositorio/TampaPoraoRepositorio.java
@@ -0,0 +1,11 @@
+package br.com.cloudport.servicoyard.vesselplanner.repositorio;
+
+import br.com.cloudport.servicoyard.vesselplanner.modelo.TampaPorao;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TampaPoraoRepositorio extends JpaRepository<TampaPorao, Long> {
+    List<TampaPorao> findByEstivagemIdOrderByBayInicialAscCodigoAsc(Long estivagemId);
+    Optional<TampaPorao> findByEstivagemIdAndCodigo(Long estivagemId, String codigo);
+}

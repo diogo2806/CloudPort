3aca53710413112f2b7a5012e536313afc225c78
Diogo Santos <diogo_2806@hotmail.com>
2026-07-17 21:17:20 -0300

    perf(inventario): carregar posição junto dos contêineres

--- backend/servico-yard/src/main/java/br/com/cloudport/servicoyard/patio/repositorio/ConteinerPatioRepositorio.java
+++ backend/servico-yard/src/main/java/br/com/cloudport/servicoyard/patio/repositorio/ConteinerPatioRepositorio.java
@@ -3,6 +3,7 @@ package br.com.cloudport.servicoyard.patio.repositorio;
 import br.com.cloudport.servicoyard.patio.modelo.ConteinerPatio;
 import java.util.List;
 import java.util.Optional;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ConteinerPatioRepositorio extends JpaRepository<ConteinerPatio, Long> {
@@ -10,6 +11,9 @@ public interface ConteinerPatioRepositorio extends JpaRepository<ConteinerPatio,
     Optional<ConteinerPatio> findByCodigo(String codigo);
 
     Optional<ConteinerPatio> findByCodigoIgnoreCase(String codigo);
+
+    @EntityGraph(attributePaths = "posicao")
+    List<ConteinerPatio> findAllByOrderByCodigoAsc();
 
     List<ConteinerPatio> findAllByOrderByPosicaoLinhaAscPosicaoColunaAsc();
 

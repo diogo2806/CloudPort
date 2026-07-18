218f7ba39c03406966c0acdf6a9413d153d2dd8e
Diogo Santos <diogo_2806@hotmail.com>
2026-07-18 06:41:55 -0300

    fix(gate): adicionar conflito funcional de billing

---
+++ backend/servico-gate/src/main/java/br/com/cloudport/servicogate/exception/ConflictException.java
@@ -0,0 +1,12 @@
+package br.com.cloudport.servicogate.exception;
+
+public class ConflictException extends BusinessException {
+
+    public ConflictException(String message) {
+        super(message);
+    }
+
+    public ConflictException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}

d1142954d5c0c963bb13cda414f4c3912a0080be
Diogo Santos <diogo_2806@hotmail.com>
2026-07-18 08:48:05 -0300

    feat(frontend): exportar pagina de instrucoes do patio

--- frontend/cloudport/src/pages/YardPages.jsx
+++ frontend/cloudport/src/pages/YardPages.jsx
@@ -5,3 +5,4 @@ export { YardMapPage, YardPositionsPage } from './yard/YardMapPages.jsx';
 export { YardReceivingPlanPage } from './yard/YardReceivingPlanPage.jsx';
 export { YardMovementsPage, YardResourcesPage, YardWorkListPage } from './yard/YardWorkPages.jsx';
 export { YardAutomationPage, YardKpiPage } from './yard/YardInsightPages.jsx';
+export { YardInstructionsPage, YardInstructionsPanel, YardInstructionCreateModal } from './yard/YardInstructionsPage.jsx';

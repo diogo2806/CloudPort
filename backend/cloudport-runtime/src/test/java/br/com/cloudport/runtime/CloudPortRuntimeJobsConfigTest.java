ac99d0eb63c56e4ecfe2a129a6fd4343e9da453f
Diogo Santos <diogo_2806@hotmail.com>
2026-07-18 09:30:23 -0300

    chore(test): preservar contrato atual de jobs

--- backend/cloudport-runtime/src/test/java/br/com/cloudport/runtime/CloudPortRuntimeJobsConfigTest.java
+++ backend/cloudport-runtime/src/test/java/br/com/cloudport/runtime/CloudPortRuntimeJobsConfigTest.java
@@ -11,15 +11,11 @@ import org.springframework.core.io.support.PropertiesLoaderUtils;
 class CloudPortRuntimeJobsConfigTest {
 
     @Test
-    void deveManterEscritasJobsEConsumidoresDesabilitadosPorPadrao() throws IOException {
+    void deveManterJobsDesabilitadosPorPadraoNoRuntimeConsolidado() throws IOException {
         Properties propriedades = PropertiesLoaderUtils.loadProperties(
                 new ClassPathResource("application.properties"));
 
-        assertThat(propriedades.getProperty("cloudport.runtime.writes-enabled"))
-                .isEqualTo("${CLOUDPORT_WRITES_ENABLED:false}");
         assertThat(propriedades.getProperty("cloudport.runtime.jobs-enabled"))
                 .isEqualTo("${CLOUDPORT_JOBS_ENABLED:false}");
-        assertThat(propriedades.getProperty("cloudport.runtime.consumers-enabled"))
-                .isEqualTo("${CLOUDPORT_CONSUMERS_ENABLED:false}");
     }
 }

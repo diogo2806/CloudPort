a099c67ac29fb434f081eeb14663ab7c24d6a2cd
Diogo Santos <diogo_2806@hotmail.com>
2026-07-18 09:07:32 -0300

    feat(yard): definir estados da divergencia de posicao

---
+++ backend/servico-yard/src/main/java/br/com/cloudport/servicoyard/patio/modelo/StatusDivergenciaPosicao.java
@@ -0,0 +1,9 @@
+package br.com.cloudport.servicoyard.patio.modelo;
+
+public enum StatusDivergenciaPosicao {
+    ABERTA,
+    EM_INVESTIGACAO,
+    CORRECAO_PENDENTE,
+    RESOLVIDA,
+    CANCELADA
+}

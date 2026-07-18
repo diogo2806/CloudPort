0478fcabb162939d42c3a2932b4b3193ff04f13b
Diogo Santos <diogo_2806@hotmail.com>
2026-07-17 22:50:52 -0300

    feat(inventory): adicionar repositório de contagens

---
+++ backend/servico-yard/src/main/java/br/com/cloudport/servicoyard/inventario/repositorio/ContagemInventarioFisicoRepositorio.java
@@ -0,0 +1,13 @@
+package br.com.cloudport.servicoyard.inventario.repositorio;
+
+import br.com.cloudport.servicoyard.inventario.modelo.ContagemInventarioFisico;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ContagemInventarioFisicoRepositorio extends JpaRepository<ContagemInventarioFisico, Long> {
+
+    List<ContagemInventarioFisico> findByStatusNotOrderByRegistradoEmDesc(
+            ContagemInventarioFisico.StatusContagem status);
+
+    List<ContagemInventarioFisico> findByLoteOrderByRegistradoEmAsc(String lote);
+}

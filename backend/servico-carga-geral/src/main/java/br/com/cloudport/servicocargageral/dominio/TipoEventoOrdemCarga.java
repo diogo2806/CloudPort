32c4980d70e9fa6626239dc8e8343c9e3f345538
Diogo Santos <diogo_2806@hotmail.com>
2026-07-18 08:13:52 -0300

    feat(carga-geral): adicionar eventos da ordem de trabalho

---
+++ backend/servico-carga-geral/src/main/java/br/com/cloudport/servicocargageral/dominio/TipoEventoOrdemCarga.java
@@ -0,0 +1,11 @@
+package br.com.cloudport.servicocargageral.dominio;
+
+public enum TipoEventoOrdemCarga {
+    CRIADA,
+    LIBERADA,
+    RECURSOS_ATRIBUIDOS,
+    INICIADA,
+    SERVICO_REGISTRADO,
+    CONCLUIDA,
+    CANCELADA
+}

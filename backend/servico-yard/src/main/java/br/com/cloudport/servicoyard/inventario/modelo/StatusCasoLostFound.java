22962a178ab0a8b55d8b23050c10274828b22344
Diogo Santos <diogo_2806@hotmail.com>
2026-07-18 09:09:10 -0300

    feat(yard): definir estados Lost Found e TBD

---
+++ backend/servico-yard/src/main/java/br/com/cloudport/servicoyard/inventario/modelo/StatusCasoLostFound.java
@@ -0,0 +1,10 @@
+package br.com.cloudport.servicoyard.inventario.modelo;
+
+public enum StatusCasoLostFound {
+    ABERTO,
+    EM_INVESTIGACAO,
+    ASSOCIADO,
+    REGULARIZADO,
+    BAIXADO,
+    ENCERRADO
+}

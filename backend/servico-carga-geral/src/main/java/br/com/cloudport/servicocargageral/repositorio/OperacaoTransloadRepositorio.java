b6e96117a8150f13bb31dd1f9079231547c43ddd
Diogo Santos <diogo_2806@hotmail.com>
2026-07-18 09:56:54 -0300

    feat: adicionar bloqueio da operação de transload

--- backend/servico-carga-geral/src/main/java/br/com/cloudport/servicocargageral/repositorio/OperacaoTransloadRepositorio.java
+++ backend/servico-carga-geral/src/main/java/br/com/cloudport/servicocargageral/repositorio/OperacaoTransloadRepositorio.java
@@ -3,11 +3,20 @@ package br.com.cloudport.servicocargageral.repositorio;
 import br.com.cloudport.servicocargageral.dominio.OperacaoTransload;
 import java.util.Optional;
 import java.util.UUID;
+import javax.persistence.LockModeType;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 
 public interface OperacaoTransloadRepositorio extends JpaRepository<OperacaoTransload, UUID> {
 
     @EntityGraph(attributePaths = "itens")
     Optional<OperacaoTransload> findByCommandId(UUID commandId);
+
+    @EntityGraph(attributePaths = "itens")
+    Optional<OperacaoTransload> findDetalhadoById(UUID id);
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @EntityGraph(attributePaths = "itens")
+    Optional<OperacaoTransload> findComBloqueioById(UUID id);
 }

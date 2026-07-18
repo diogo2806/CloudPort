c5c80627dcb3a62cbb41896c1b871d4fabc444d7
Diogo Santos <diogo_2806@hotmail.com>
2026-07-18 08:42:56 -0300

    fix(carga-geral): explicitar caminho do id da operação

--- backend/servico-carga-geral/src/main/java/br/com/cloudport/servicocargageral/repositorio/ComandoExecucaoStuffUnstuffRepositorio.java
+++ backend/servico-carga-geral/src/main/java/br/com/cloudport/servicocargageral/repositorio/ComandoExecucaoStuffUnstuffRepositorio.java
@@ -8,5 +8,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface ComandoExecucaoStuffUnstuffRepositorio
         extends JpaRepository<ComandoExecucaoStuffUnstuff, UUID> {
 
-    Optional<ComandoExecucaoStuffUnstuff> findByOperacaoIdAndCommandId(UUID operacaoId, UUID commandId);
+    Optional<ComandoExecucaoStuffUnstuff> findByOperacao_IdAndCommandId(UUID operacaoId, UUID commandId);
 }

6c3b118ef72ea80fd0d24a86a497942e96db7b32
Diogo Santos <diogo_2806@hotmail.com>
2026-07-17 22:49:51 -0300

    feat(carga-geral): adicionar aplicação do módulo

---
+++ backend/servico-carga-geral/src/main/java/br/com/cloudport/servicocargageral/ServicoCargaGeralApplication.java
@@ -0,0 +1,12 @@
+package br.com.cloudport.servicocargageral;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicoCargaGeralApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServicoCargaGeralApplication.class, args);
+    }
+}

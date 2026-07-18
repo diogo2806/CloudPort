607c354a248eed95aff54b15bb6ff043777e4f25
Diogo Santos <diogo_2806@hotmail.com>
2026-07-18 06:37:05 -0300

    fix(carga-geral): prioriza handler de conflito conhecido

--- backend/servico-carga-geral/src/main/java/br/com/cloudport/servicocargageral/comum/erro/TratadorConflitosCargaGeral.java
+++ backend/servico-carga-geral/src/main/java/br/com/cloudport/servicocargageral/comum/erro/TratadorConflitosCargaGeral.java
@@ -6,12 +6,15 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.UUID;
 import javax.servlet.http.HttpServletRequest;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@Order(Ordered.HIGHEST_PRECEDENCE)
 @RestControllerAdvice(assignableTypes = CargaGeralControlador.class)
 public class TratadorConflitosCargaGeral {
 

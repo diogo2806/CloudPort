8e0a4eff12b5e2fe4d9b3ef43a7e37a0114e65c8
Diogo Santos <diogo_2806@hotmail.com>
2026-07-20 14:13:57 -0300

    feat(yard): exportar painel Yard Impact

--- frontend/cloudport/src/pages/YardPages.jsx
+++ frontend/cloudport/src/pages/YardPages.jsx
@@ -3,6 +3,7 @@ import './yard/YardOperationalExtensions.css';
 
 export { YardMapPage, YardPositionsPage } from './yard/YardMapPages.jsx';
 export { YardReceivingPlanPage } from './yard/YardReceivingPlanPage.jsx';
+export { YardImpactPage } from './yard/YardImpactPage.jsx';
 export { YardMovementsPage, YardResourcesPage, YardWorkListPage } from './yard/YardWorkPages.jsx';
 export { YardAutomationPage, YardKpiPage } from './yard/YardInsightPages.jsx';
 export { YardInstructionsPage, YardInstructionsPanel, YardInstructionCreateModal } from './yard/YardInstructionsPage.jsx';

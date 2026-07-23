b0cbaf8a908b2e7f3ab864eb438f4823a2a2a6cf
Diogo Santos <diogo_2806@hotmail.com>
2026-07-23 12:05:33 -0300

    feat: adicionar causas do purgatório operacional

---
+++ backend/servico-yard/src/main/java/br/com/cloudport/servicoyard/patio/purgatorio/modelo/CausaPurgatorioWorkInstruction.java
@@ -0,0 +1,12 @@
+package br.com.cloudport.servicoyard.patio.purgatorio.modelo;
+
+public enum CausaPurgatorioWorkInstruction {
+    DESTINO,
+    INVENTARIO,
+    CHE,
+    POSICAO,
+    SEQUENCIA,
+    DEPENDENCIA,
+    TELEMETRIA,
+    INTEGRACAO
+}

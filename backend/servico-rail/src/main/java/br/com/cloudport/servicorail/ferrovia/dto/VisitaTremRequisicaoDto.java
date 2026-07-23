798bc8147969a10aa37b4a567f06e12a18d56fa7
Diogo Santos <diogo_2806@hotmail.com>
2026-07-23 04:07:05 -0300

    feat(rail): permitir selecionar trem mestre na visita

--- backend/servico-rail/src/main/java/br/com/cloudport/servicorail/ferrovia/dto/VisitaTremRequisicaoDto.java
+++ backend/servico-rail/src/main/java/br/com/cloudport/servicorail/ferrovia/dto/VisitaTremRequisicaoDto.java
@@ -12,6 +12,8 @@ import javax.validation.constraints.Size;
 
 public class VisitaTremRequisicaoDto {
 
+    private Long tremMestreId;
+
     @NotBlank
     @Size(max = 40)
     private String identificadorTrem;
@@ -38,67 +40,22 @@ public class VisitaTremRequisicaoDto {
     @Valid
     private List<VagaoVisitaRequisicaoDto> listaVagoes = new ArrayList<>();
 
-    public String getIdentificadorTrem() {
-        return identificadorTrem;
-    }
-
-    public void setIdentificadorTrem(String identificadorTrem) {
-        this.identificadorTrem = ValidacaoEntradaUtil.limparTexto(identificadorTrem);
-    }
-
-    public String getOperadoraFerroviaria() {
-        return operadoraFerroviaria;
-    }
-
-    public void setOperadoraFerroviaria(String operadoraFerroviaria) {
-        this.operadoraFerroviaria = ValidacaoEntradaUtil.limparTexto(operadoraFerroviaria);
-    }
-
-    public LocalDateTime getHoraChegadaPrevista() {
-        return horaChegadaPrevista;
-    }
-
-    public void setHoraChegadaPrevista(LocalDateTime horaChegadaPrevista) {
-        this.horaChegadaPrevista = horaChegadaPrevista;
-    }
-
-    public LocalDateTime getHoraPartidaPrevista() {
-        return horaPartidaPrevista;
-    }
-
-    public void setHoraPartidaPrevista(LocalDateTime horaPartidaPrevista) {
-        this.horaPartidaPrevista = horaPartidaPrevista;
-    }
-
-    public StatusVisitaTrem getStatusVisita() {
-        return statusVisita;
-    }
-
-    public void setStatusVisita(StatusVisitaTrem statusVisita) {
-        this.statusVisita = statusVisita;
-    }
-
-    public List<OperacaoConteinerVisitaRequisicaoDto> getListaDescarga() {
-        return listaDescarga;
-    }
-
-    public void setListaDescarga(List<OperacaoConteinerVisitaRequisicaoDto> listaDescarga) {
-        this.listaDescarga = listaDescarga != null ? new ArrayList<>(listaDescarga) : new ArrayList<>();
-    }
-
-    public List<OperacaoConteinerVisitaRequisicaoDto> getListaCarga() {
-        return listaCarga;
-    }
-
-    public void setListaCarga(List<OperacaoConteinerVisitaRequisicaoDto> listaCarga) {
-        this.listaCarga = listaCarga != null ? new ArrayList<>(listaCarga) : new ArrayList<>();
-    }
-
-    public List<VagaoVisitaRequisicaoDto> getListaVagoes() {
-        return listaVagoes;
-    }
-
-    public void setListaVagoes(List<VagaoVisitaRequisicaoDto> listaVagoes) {
-        this.listaVagoes = listaVagoes != null ? new ArrayList<>(listaVagoes) : new ArrayList<>();
-    }
+    public Long getTremMestreId() { return tremMestreId; }
+    public void setTremMestreId(Long tremMestreId) { this.tremMestreId = tremMestreId; }
+    public String getIdentificadorTrem() { return identificadorTrem; }
+    public void setIdentificadorTrem(String identificadorTrem) { this.identificadorTrem = ValidacaoEntradaUtil.limparTexto(identificadorTrem); }
+    public String getOperadoraFerroviaria() { return operadoraFerroviaria; }
+    public void setOperadoraFerroviaria(String operadoraFerroviaria) { this.operadoraFerroviaria = ValidacaoEntradaUtil.limparTexto(operadoraFerroviaria); }
+    public LocalDateTime getHoraChegadaPrevista() { return horaChegadaPrevista; }
+    public void setHoraChegadaPrevista(LocalDateTime horaChegadaPrevista) { this.horaChegadaPrevista = horaChegadaPrevista; }
+    public LocalDateTime getHoraPartidaPrevista() { return horaPartidaPrevista; }
+    public void setHoraPartidaPrevista(LocalDateTime horaPartidaPrevista) { this.horaPartidaPrevista = horaPartidaPrevista; }
+    public StatusVisitaTrem getStatusVisita() { return statusVisita; }
+    public void setStatusVisita(StatusVisitaTrem statusVisita) { this.statusVisita = statusVisita; }
+    public List<OperacaoConteinerVisitaRequisicaoDto> getListaDescarga() { return listaDescarga; }
+    public void setListaDescarga(List<OperacaoConteinerVisitaRequisicaoDto> listaDescarga) { this.listaDescarga = listaDescarga != null ? new ArrayList<>(listaDescarga) : new ArrayList<>(); }
+    public List<OperacaoConteinerVisitaRequisicaoDto> getListaCarga() { return listaCarga; }
+    public void setListaCarga(List<OperacaoConteinerVisitaRequisicaoDto> listaCarga) { this.listaCarga = listaCarga != null ? new ArrayList<>(listaCarga) : new ArrayList<>(); }
+    public List<VagaoVisitaRequisicaoDto> getListaVagoes() { return listaVagoes; }
+    public void setListaVagoes(List<VagaoVisitaRequisicaoDto> listaVagoes) { this.listaVagoes = listaVagoes != null ? new ArrayList<>(listaVagoes) : new ArrayList<>(); }
 }

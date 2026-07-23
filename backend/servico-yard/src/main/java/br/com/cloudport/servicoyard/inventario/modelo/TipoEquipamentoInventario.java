dc6911486e70e6eb9d5f1d6b0f823ebae1b388f3
Diogo Santos <diogo_2806@hotmail.com>
2026-07-23 10:19:24 -0300

    feat: evoluir modelo de tipos ISO

--- backend/servico-yard/src/main/java/br/com/cloudport/servicoyard/inventario/modelo/TipoEquipamentoInventario.java
+++ backend/servico-yard/src/main/java/br/com/cloudport/servicoyard/inventario/modelo/TipoEquipamentoInventario.java
@@ -6,9 +6,12 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.PrePersist;
 import javax.persistence.PreUpdate;
 import javax.persistence.Table;
@@ -17,12 +20,7 @@ import javax.persistence.Table;
 @Table(name = "tipo_equipamento_inventario")
 public class TipoEquipamentoInventario {
 
-    public enum CategoriaEquipamento {
-        CONTEINER,
-        CHASSI,
-        CARRETA,
-        ACESSORIO
-    }
+    public enum CategoriaEquipamento { CONTEINER, CHASSI, CARRETA, ACESSORIO }
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -38,8 +36,18 @@ public class TipoEquipamentoInventario {
     @Column(name = "categoria", nullable = false, length = 30)
     private CategoriaEquipamento categoria;
 
-    @Column(name = "codigo_iso", length = 10)
+    @Column(name = "codigo_iso", length = 4, unique = true)
     private String codigoIso;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "grupo_iso_id")
+    private GrupoIsoEquipamento grupoIso;
+
+    @Column(name = "arquetipo_iso", length = 30)
+    private String arquetipoIso;
+
+    @Column(name = "indicador_arquetipo", nullable = false)
+    private boolean indicadorArquetipo;
 
     @Column(name = "comprimento_mm")
     private Integer comprimentoMm;
@@ -62,8 +70,17 @@ public class TipoEquipamentoInventario {
     @Column(name = "grupo_equivalencia", length = 60)
     private String grupoEquivalencia;
 
+    @Column(name = "provisorio_edi", nullable = false)
+    private boolean provisorioEdi;
+
     @Column(name = "ativo", nullable = false)
     private boolean ativo = true;
+
+    @Column(name = "criado_por", length = 100)
+    private String criadoPor;
+
+    @Column(name = "atualizado_por", length = 100)
+    private String atualizadoPor;
 
     @Column(name = "criado_em", nullable = false)
     private LocalDateTime criadoEm;
@@ -79,119 +96,46 @@ public class TipoEquipamentoInventario {
     }
 
     @PreUpdate
-    public void preUpdate() {
-        atualizadoEm = LocalDateTime.now();
-    }
+    public void preUpdate() { atualizadoEm = LocalDateTime.now(); }
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getCodigo() {
-        return codigo;
-    }
-
-    public void setCodigo(String codigo) {
-        this.codigo = codigo;
-    }
-
-    public String getDescricao() {
-        return descricao;
-    }
-
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
-
-    public CategoriaEquipamento getCategoria() {
-        return categoria;
-    }
-
-    public void setCategoria(CategoriaEquipamento categoria) {
-        this.categoria = categoria;
-    }
-
-    public String getCodigoIso() {
-        return codigoIso;
-    }
-
-    public void setCodigoIso(String codigoIso) {
-        this.codigoIso = codigoIso;
-    }
-
-    public Integer getComprimentoMm() {
-        return comprimentoMm;
-    }
-
-    public void setComprimentoMm(Integer comprimentoMm) {
-        this.comprimentoMm = comprimentoMm;
-    }
-
-    public Integer getLarguraMm() {
-        return larguraMm;
-    }
-
-    public void setLarguraMm(Integer larguraMm) {
-        this.larguraMm = larguraMm;
-    }
-
-    public Integer getAlturaMm() {
-        return alturaMm;
-    }
-
-    public void setAlturaMm(Integer alturaMm) {
-        this.alturaMm = alturaMm;
-    }
-
-    public BigDecimal getTaraKg() {
-        return taraKg;
-    }
-
-    public void setTaraKg(BigDecimal taraKg) {
-        this.taraKg = taraKg;
-    }
-
-    public BigDecimal getCapacidadeKg() {
-        return capacidadeKg;
-    }
-
-    public void setCapacidadeKg(BigDecimal capacidadeKg) {
-        this.capacidadeKg = capacidadeKg;
-    }
-
-    public boolean isRefrigerado() {
-        return refrigerado;
-    }
-
-    public void setRefrigerado(boolean refrigerado) {
-        this.refrigerado = refrigerado;
-    }
-
-    public String getGrupoEquivalencia() {
-        return grupoEquivalencia;
-    }
-
-    public void setGrupoEquivalencia(String grupoEquivalencia) {
-        this.grupoEquivalencia = grupoEquivalencia;
-    }
-
-    public boolean isAtivo() {
-        return ativo;
-    }
-
-    public void setAtivo(boolean ativo) {
-        this.ativo = ativo;
-    }
-
-    public LocalDateTime getCriadoEm() {
-        return criadoEm;
-    }
-
-    public LocalDateTime getAtualizadoEm() {
-        return atualizadoEm;
-    }
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+    public String getCodigo() { return codigo; }
+    public void setCodigo(String codigo) { this.codigo = codigo; }
+    public String getDescricao() { return descricao; }
+    public void setDescricao(String descricao) { this.descricao = descricao; }
+    public CategoriaEquipamento getCategoria() { return categoria; }
+    public void setCategoria(CategoriaEquipamento categoria) { this.categoria = categoria; }
+    public String getCodigoIso() { return codigoIso; }
+    public void setCodigoIso(String codigoIso) { this.codigoIso = codigoIso; }
+    public GrupoIsoEquipamento getGrupoIso() { return grupoIso; }
+    public void setGrupoIso(GrupoIsoEquipamento grupoIso) { this.grupoIso = grupoIso; }
+    public String getArquetipoIso() { return arquetipoIso; }
+    public void setArquetipoIso(String arquetipoIso) { this.arquetipoIso = arquetipoIso; }
+    public boolean isIndicadorArquetipo() { return indicadorArquetipo; }
+    public void setIndicadorArquetipo(boolean indicadorArquetipo) { this.indicadorArquetipo = indicadorArquetipo; }
+    public Integer getComprimentoMm() { return comprimentoMm; }
+    public void setComprimentoMm(Integer comprimentoMm) { this.comprimentoMm = comprimentoMm; }
+    public Integer getLarguraMm() { return larguraMm; }
+    public void setLarguraMm(Integer larguraMm) { this.larguraMm = larguraMm; }
+    public Integer getAlturaMm() { return alturaMm; }
+    public void setAlturaMm(Integer alturaMm) { this.alturaMm = alturaMm; }
+    public BigDecimal getTaraKg() { return taraKg; }
+    public void setTaraKg(BigDecimal taraKg) { this.taraKg = taraKg; }
+    public BigDecimal getCapacidadeKg() { return capacidadeKg; }
+    public void setCapacidadeKg(BigDecimal capacidadeKg) { this.capacidadeKg = capacidadeKg; }
+    public boolean isRefrigerado() { return refrigerado; }
+    public void setRefrigerado(boolean refrigerado) { this.refrigerado = refrigerado; }
+    public String getGrupoEquivalencia() { return grupoEquivalencia; }
+    public void setGrupoEquivalencia(String grupoEquivalencia) { this.grupoEquivalencia = grupoEquivalencia; }
+    public boolean isProvisorioEdi() { return provisorioEdi; }
+    public void setProvisorioEdi(boolean provisorioEdi) { this.provisorioEdi = provisorioEdi; }
+    public boolean isAtivo() { return ativo; }
+    public void setAtivo(boolean ativo) { this.ativo = ativo; }
+    public String getCriadoPor() { return criadoPor; }
+    public void setCriadoPor(String criadoPor) { this.criadoPor = criadoPor; }
+    public String getAtualizadoPor() { return atualizadoPor; }
+    public void setAtualizadoPor(String atualizadoPor) { this.atualizadoPor = atualizadoPor; }
+    public LocalDateTime getCriadoEm() { return criadoEm; }
+    public LocalDateTime getAtualizadoEm() { return atualizadoEm; }
 }

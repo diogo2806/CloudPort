a70ff625b0424c67cb4b25f115d63c9656fec094
Diogo Santos <diogo_2806@hotmail.com>
2026-07-23 10:10:24 -0300

    refactor(yard): preservar controller de otimização existente

--- backend/servico-yard/src/main/java/br/com/cloudport/servicoyard/patio/controller/OtimizacaoYardController.java
+++ backend/servico-yard/src/main/java/br/com/cloudport/servicoyard/patio/controller/OtimizacaoYardController.java
@@ -2,14 +2,11 @@ package br.com.cloudport.servicoyard.patio.controller;
 
 import br.com.cloudport.servicoyard.patio.dto.ContainerOtimizacaoDto;
 import br.com.cloudport.servicoyard.patio.dto.PosicaoOtimizadaDto;
-import br.com.cloudport.servicoyard.patio.dto.RespostaAlocacaoIntegradaYardDto;
-import br.com.cloudport.servicoyard.patio.servico.AlocacaoIntegradaYardServico;
 import br.com.cloudport.servicoyard.patio.servico.OptimizadorYardService;
 import java.util.List;
 import javax.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,35 +15,31 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/patio/otimizacao")
-@PreAuthorize("hasAnyRole('ADMIN_PORTO','PLANEJADOR')")
 public class OtimizacaoYardController {
 
     private final OptimizadorYardService otimizadorYard;
-    private final AlocacaoIntegradaYardServico alocacaoIntegrada;
 
-    public OtimizacaoYardController(OptimizadorYardService otimizadorYard,
-                                     AlocacaoIntegradaYardServico alocacaoIntegrada) {
+    public OtimizacaoYardController(OptimizadorYardService otimizadorYard) {
         this.otimizadorYard = otimizadorYard;
-        this.alocacaoIntegrada = alocacaoIntegrada;
     }
 
     @PostMapping("/alocar")
     public ResponseEntity<List<PosicaoOtimizadaDto>> otimizarAlocacao(
             @Valid @RequestBody List<ContainerOtimizacaoDto> containers) {
-        return ResponseEntity.status(HttpStatus.OK).body(otimizadorYard.otimizarAlocacao(containers));
+
+        List<PosicaoOtimizadaDto> posicoes = otimizadorYard.otimizarAlocacao(containers);
+        return ResponseEntity.status(HttpStatus.OK).body(posicoes);
     }
 
     @PostMapping("/alocar-por-navio")
     public ResponseEntity<List<PosicaoOtimizadaDto>> otimizarAlocacaoPorNavio(
             @Valid @RequestBody List<ContainerOtimizacaoDto> containers,
             @RequestParam(defaultValue = "999999") Integer distanciaMaximaAoBerco) {
-        return ResponseEntity.status(HttpStatus.OK).body(
-                otimizadorYard.otimizarAlocacaoPorNavio(containers, distanciaMaximaAoBerco));
-    }
 
-    @PostMapping("/alocar-e-validar")
-    public ResponseEntity<RespostaAlocacaoIntegradaYardDto> alocarEValidar(
-            @Valid @RequestBody List<ContainerOtimizacaoDto> containers) {
-        return ResponseEntity.ok(alocacaoIntegrada.alocar(containers));
+        List<PosicaoOtimizadaDto> posicoes = otimizadorYard.otimizarAlocacaoPorNavio(
+                containers,
+                distanciaMaximaAoBerco
+        );
+        return ResponseEntity.status(HttpStatus.OK).body(posicoes);
     }
 }

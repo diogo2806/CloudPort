2c4427ee77cf729ea30c4fc049f67185862407c8
Diogo Santos <diogo_2806@hotmail.com>
2026-07-24 09:17:10 -0300

    feat(yard): definir regras dos avisos de estivagem

---
+++ backend/servico-yard/src/main/java/br/com/cloudport/servicoyard/patio/modelo/TipoRegraEstivagemPatio.java
@@ -0,0 +1,13 @@
+package br.com.cloudport.servicoyard.patio.modelo;
+
+public enum TipoRegraEstivagemPatio {
+    PESO,
+    ALTURA,
+    TIPO,
+    REEFER,
+    PERIGOSO,
+    CAPACIDADE,
+    RESERVA,
+    APOIO,
+    REGRA_PILHA
+}

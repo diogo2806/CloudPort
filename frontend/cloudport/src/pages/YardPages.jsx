1a5499fabd43b4eb102d2ea50a2a8cbeab0a0a93
Diogo Santos <diogo_2806@hotmail.com>
2026-07-24 09:27:07 -0300

    feat(front): exportar tela de avisos de estivagem

--- frontend/cloudport/src/pages/YardPages.jsx
+++ frontend/cloudport/src/pages/YardPages.jsx
@@ -7,3 +7,4 @@ export { YardImpactPage } from './yard/YardImpactPage.jsx';
 export { YardMovementsPage, YardResourcesPage, YardWorkListPage } from './yard/YardWorkPages.jsx';
 export { YardAutomationPage, YardKpiPage } from './yard/YardInsightPages.jsx';
 export { YardInstructionsPage, YardInstructionsPanel, YardInstructionCreateModal } from './yard/YardInstructionsPage.jsx';
+export { YardStowageWarningsPage } from './yard/YardStowageWarningsPage.jsx';
